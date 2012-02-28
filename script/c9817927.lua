6e3fab5e42601f804ff0e78e34edd32cc1a8f149
argon.sun <argon.sun@gmail.com>
2012-02-28 21:36:23 +0800

    fix

---
+++ script/c9817927.lua
@@ -0,0 +1,19 @@
+--逆ギレパンダ
+function c9817927.initial_effect(c)
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(c9817927.val)
+	c:RegisterEffect(e1)
+	--pierce
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e2)
+end
+function c9817927.val(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE)*500
+end
