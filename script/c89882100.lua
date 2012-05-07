2304847fccf1b441caeb6993db85dd906b7fd62e
argon.sun <argon.sun@gmail.com>
2012-05-07 21:14:33 +0800

    fix

--- script/c89882100.lua
+++ script/c89882100.lua
@@ -23,7 +23,7 @@ function c89882100.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c89882100.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsFacedown() and tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
