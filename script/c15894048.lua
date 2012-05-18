12b0852b83a34e7f1eba5f80ac3e65c3a959e67c
argon.sun <argon.sun@gmail.com>
2012-05-18 19:55:22 +0800

    new scripts

---
+++ script/c15894048.lua
@@ -0,0 +1,17 @@
+--究極恐獣
+function c15894048.initial_effect(c)
+	--attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_FIRST_ATTACK)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_MUST_ATTACK)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_ATTACK_ALL)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
