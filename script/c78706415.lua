36749e0d5346fb9224193af927687b461f2fda2f
argon.sun <argon.sun@gmail.com>
2012-10-30 21:41:21 +0800

    fix

--- script/c78706415.lua
+++ script/c78706415.lua
@@ -4,13 +4,18 @@ function c78706415.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP)
+	e1:SetTarget(c78706415.target)
 	e1:SetOperation(c78706415.operation)
 	c:RegisterEffect(e1)
+end
+function c78706415.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetFieldGroup(tp,0x1e,0x1e)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
 end
 function c78706415.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetFieldGroup(tp,0x1e,0x1e)
-	if c:IsStatus(STATUS_BATTLE_DESTROYED) then g:RemoveCard(c) end
 	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
 	Duel.ShuffleDeck(tp)
 	Duel.ShuffleDeck(1-tp)
