cebb2ace11ad0e3cc94a961ae32272211b59abac
salix5 <salix5@livemail.tw>
2013-06-13 23:26:52 +0800

    fix: 47942531, 6614221, 8794435, 88071625, 73333463
    47942531  偉大魔獣 ガーゼット
    6614221  霧の王
    8794435  合成魔獣 ガーゼット
    88071625 Ｔｈｅ　ｔｙｒａｎｔ　ＮＥＰＴＵＮＥ
    The atk setting/updating continuous effect was given at SUMMON_SUCCESS
    before, which may cause some problem with effects triggered at te same
    time.(ex. 王虎ワンフー, traps like 奈落の落とし穴)
    
    Now the atk setting/updating continuous effect is given right after
    tribute check, which will be ready at SUMMON_SUCCESS.
    Also, the RESET_EVENT is changed into 0xff0000.
    
    P.S.
    Current script uses text_attack when calculating atk, so the trap
    monster should have their own atk and def data in cards.cdb
    Here is a brief list of trap mosters:
    
    28649820 アポピスの化神  			1600/1800
    3129635 カース・オブ・スタチュー 		1800/1000
    70406920 機械王－Ｂ.Ｃ.３０００ 		1000/1000
    13955608 機動砦 ストロング・ホールド		0/2000
    21843307 コピー・ナイト 			0/0
    90440725 サイバー・シャドー・ガードナー 	?/?
    92099232 シェイプシスター  			0/0
    79852326 死霊ゾーマ			1800/500
    49514333 ソウル・オブ・スタチュー          	1000/1800
    26905245 メタル・リフレクト・スライム	0/3000
    
    73333463  アーマロイドガイデンゴー
    Now the card name in the script is chaned into the correct one.

--- script/c47942531.lua
+++ script/c47942531.lua
@@ -1,33 +1,24 @@
 --偉大魔獣 ガーゼット
 function c47942531.initial_effect(c)
-	--atk
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetOperation(c47942531.atop)
-	c:RegisterEffect(e1)
 	--tribute check
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_MATERIAL_CHECK)
-	e2:SetValue(c47942531.valcheck)
-	e2:SetLabelObject(e1)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_MATERIAL_CHECK)
+	e1:SetValue(c47942531.valcheck)
+	c:RegisterEffect(e1)
 end
 function c47942531.valcheck(e,c)
 	local tc=c:GetMaterial():GetFirst()
-	local atk=tc:GetTextAttack()
+	local atk=0
+	if tc then atk=tc:GetTextAttack()*2 end
 	if atk<0 then atk=0 end
-	e:GetLabelObject():SetLabel(atk*2)
-end
-function c47942531.atop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+	--atk continuous effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SET_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(e:GetLabel())
-	e1:SetReset(RESET_EVENT+0x1ff0000)
-	c:RegisterEffect(e1)
+	e1:SetValue(atk)
+	e1:SetReset(RESET_EVENT+0xff0000)
+	c:RegisterEffect(e1)	
 end
