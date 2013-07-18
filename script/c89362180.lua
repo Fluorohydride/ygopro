daefab5dfb539513de5196ca3a9553f48d06eb42
argon <argon.sun@gmail.com>
2013-07-18 20:38:37 +0800

    new

---
+++ script/c89362180.lua
@@ -0,0 +1,56 @@
+--ナチュラル・ボーン・サウルス
+function c89362180.initial_effect(c)
+	aux.EnableDualAttribute(c)
+	--prop change
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(aux.IsDualState)
+	e1:SetValue(ATTRIBUTE_EARTH)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CHANGE_RACE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(aux.IsDualState)
+	e2:SetValue(RACE_DINOSAUR)
+	c:RegisterEffect(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(89362180,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCondition(c89362180.spcon)
+	e3:SetTarget(c89362180.sptg)
+	e3:SetOperation(c89362180.spop)
+	c:RegisterEffect(e3)
+end
+function c89362180.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsDualState() then return false end
+	local bc=c:GetBattleTarget()
+	if not c:IsRelateToBattle() or c:IsFacedown() then return false end
+	return bc:IsLocation(LOCATION_GRAVE) and bc:IsType(TYPE_MONSTER)
+end
+function c89362180.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local bc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and bc:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetTargetCard(bc)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,bc,1,0,LOCATION_GRAVE)
+end
+function c89362180.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENCE)>0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_RACE)
+		e1:SetValue(RACE_ZOMBIE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+	end
+end
