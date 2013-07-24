9db46431af70fc128403d22594249ab9d2de39ac
argon <argon.sun@gmail.com>
2013-07-24 11:02:29 +0800

    fix

--- script/c92729410.lua
+++ script/c92729410.lua
@@ -1,69 +1,69 @@
 --×ÓÀê¤Ý¤ó¤Ý¤³
-function c82725410.initial_effect(c)
+function c92729410.initial_effect(c)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(82725410,0))
+	e2:SetDescription(aux.Stringid(92729410,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCost(c82725410.spcost)
-	e2:SetTarget(c82725410.sptg)
-	e2:SetOperation(c82725410.spop)
+	e2:SetCost(c92729410.spcost)
+	e2:SetTarget(c92729410.sptg)
+	e2:SetOperation(c92729410.spop)
 	c:RegisterEffect(e2)
-	if not c82725410.global_check then
-		c82725410.global_check=true
+	if not c92729410.global_check then
+		c92729410.global_check=true
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge1:SetOperation(c82725410.checkop)
+		ge1:SetOperation(c92729410.checkop)
 		Duel.RegisterEffect(ge1,0)
 		local ge2=Effect.CreateEffect(c)
 		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge2:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-		ge2:SetOperation(c82725410.clear)
+		ge2:SetOperation(c92729410.clear)
 		Duel.RegisterEffect(ge2,0)
 	end
 end
-function c82725410.checkop(e,tp,eg,ep,ev,re,r,rp)
+function c92729410.checkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	while tc do
 		if not tc:IsRace(RACE_BEAST) then
-			c82725410[tc:GetSummonPlayer()]=false
+			c92729410[tc:GetSummonPlayer()]=false
 		end
 		tc=eg:GetNext()
 	end
 end
-function c82725410.clear(e,tp,eg,ep,ev,re,r,rp)
-	c82725410[0]=true
-	c82725410[1]=true
+function c92729410.clear(e,tp,eg,ep,ev,re,r,rp)
+	c92729410[0]=true
+	c92729410[1]=true
 end
-function c82725410.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return c82725410[tp] end
+function c92729410.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return c92729410[tp] end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c82725410.splimit)
+	e1:SetTarget(c92729410.splimit)
 	Duel.RegisterEffect(e1,tp)
 end
-function c82725410.splimit(e,c)
+function c92729410.splimit(e,c)
 	return not c:IsRace(RACE_BEAST)
 end
-function c82725410.filter(c,e,tp)
-	return c:GetCode()~=82725410 and c:GetLevel()==2 and c:IsRace(RACE_BEAST)
+function c92729410.filter(c,e,tp)
+	return c:GetCode()~=92729410 and c:GetLevel()==2 and c:IsRace(RACE_BEAST)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN)
 end
-function c82725410.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c92729410.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c82725410.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c92729410.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function c82725410.spop(e,tp,eg,ep,ev,re,r,rp)
+function c92729410.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c82725410.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c92729410.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENCE)
 		Duel.ConfirmCards(1-tp,g)
