19b97989a88c1a38393a60212964d827466f1fb8
argon <argon.sun@gmail.com>
2013-08-13 18:26:05 +0800

    fix

--- script/c27971137.lua
+++ script/c27971137.lua
@@ -2,8 +2,7 @@
 function c27971137.initial_effect(c)
 	--atkup
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(27971137,0))
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetOperation(c27971137.atkop)
 	c:RegisterEffect(e1)
