85405eed6b06d63ea1416b3c6a3e4ea671a56efe
VanillaSalt <vanillasalt5197@gmail.com>
2013-10-05 19:23:24 +0900

    fix

--- script/c2948263.lua
+++ script/c2948263.lua
@@ -1,5 +1,5 @@
 --ゴゴゴゴーレム－GF
-function c2949263.initial_effect(c)
+function c2948263.initial_effect(c)
 	c:EnableReviveLimit()
 	--cannot special summon
 	local e1=Effect.CreateEffect(c)
@@ -13,36 +13,36 @@ function c2949263.initial_effect(c)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_HAND)
-	e2:SetCondition(c2949263.spcon)
-	e2:SetOperation(c2949263.spop)
+	e2:SetCondition(c2948263.spcon)
+	e2:SetOperation(c2948263.spop)
 	c:RegisterEffect(e2)
 	--reduce
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e3:SetCondition(c2949263.rdcon)
-	e3:SetOperation(c2949263.rdop)
+	e3:SetCondition(c2948263.rdcon)
+	e3:SetOperation(c2948263.rdop)
 	c:RegisterEffect(e3)
 	--disable
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(2949263,0))
+	e4:SetDescription(aux.Stringid(2948263,0))
 	e4:SetCategory(CATEGORY_DISABLE)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_F)
 	e4:SetCode(EVENT_CHAINING)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
-	e4:SetCondition(c2949263.discon)
-	e4:SetTarget(c2949263.distg)
-	e4:SetOperation(c2949263.disop)
+	e4:SetCondition(c2948263.discon)
+	e4:SetTarget(c2948263.distg)
+	e4:SetOperation(c2948263.disop)
 	c:RegisterEffect(e4)
 end
-function c2949263.spcon(e,c)
+function c2948263.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>-1
 		and Duel.CheckReleaseGroup(c:GetControler(),Card.IsSetCard,1,nil,0x59)
 end
-function c2949263.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function c2948263.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.SelectReleaseGroup(c:GetControler(),Card.IsSetCard,1,1,nil,0x59)
 	Duel.Release(g,REASON_COST)
 	local atk=g:GetFirst():GetBaseAttack()
@@ -54,22 +54,22 @@ function c2949263.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	e1:SetReset(RESET_EVENT+0xff0000)
 	c:RegisterEffect(e1)
 end
-function c2949263.rdcon(e,tp,eg,ep,ev,re,r,rp)
+function c2948263.rdcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp
 end
-function c2949263.rdop(e,tp,eg,ep,ev,re,r,rp)
+function c2948263.rdop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ChangeBattleDamage(ep,ev/2)
 end
-function c2949263.discon(e,tp,eg,ep,ev,re,r,rp)
+function c2948263.discon(e,tp,eg,ep,ev,re,r,rp)
 	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
 	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainDisablable(ev)
 		and rp~=tp and re:IsActiveType(TYPE_MONSTER) and loc==LOCATION_MZONE
 end
-function c2949263.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c2948263.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 end
-function c2949263.disop(e,tp,eg,ep,ev,re,r,rp)
+function c2948263.disop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFacedown() or c:GetAttack()<1500 or not c:IsRelateToEffect(e) or Duel.GetCurrentChain()~=ev+1 or c:IsStatus(STATUS_BATTLE_DESTROYED) then
 		return
