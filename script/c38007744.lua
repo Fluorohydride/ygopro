667a831c0c6f85682b95a1aa59bef47abf492c61
salix5 <salix5@livemail.tw>
2013-11-18 12:22:56 +0800

    fix
    24861088 先史遺産ネブラ・ディスク
    There are 2 effects, and only the 2nd effect needs global check.
    The global check flag effect is changed into 24861089, and flag 24861088
    is changed into the form which once-per-turn monsters use.
    
    38007744 先史遺産モアイキャリア
    The condition is ALL cards on field, not only monster cards.

--- script/c38007744.lua
+++ script/c38007744.lua
@@ -11,7 +11,7 @@ function c38007744.initial_effect(c)
 end
 function c38007744.spcon(e,c)
 	if c==nil then return true end
-	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0)==0
-		and Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE)>0
+	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_ONFIELD,0)==0
+		and Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_ONFIELD)>0
 		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
