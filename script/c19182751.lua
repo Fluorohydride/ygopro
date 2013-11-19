fa6df5cff595099534d840dd5e8e89637e3c9d97
VanillaSalt <vanillasalt5197@gmail.com>
2013-11-20 08:02:36 +0900

    fix

--- script/c19182751.lua
+++ script/c19182751.lua
@@ -15,8 +15,8 @@ function c19182751.regop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetTarget(c19182751.target)
 	e1:SetCountLimit(1)
+	e1:SetTarget(c19182751.target)
 	e1:SetOperation(c19182751.operation)
 	e1:SetReset(RESET_EVENT+0x16c0000+RESET_PHASE+PHASE_END)
 	e:GetHandler():RegisterEffect(e1)
@@ -27,6 +27,7 @@ end
 function c19182751.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c19182751.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	e:GetHandler():ResetNegateEffect(25789292,97268402)
 end
 function c19182751.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
