d388794353d2bd7c6cb94417e8205574c45922e8
sidschingis <sijing@gmx.net>
2013-12-02 17:03:58 +0100

    fix
    fixed not correctly resolving when the Target is Flipped Face-Down
    before Resolution

--- script/c42874792.lua
+++ script/c42874792.lua
@@ -25,7 +25,7 @@ function c42874792.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c42874792.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)~=0 then
+	if tc:IsRelateToEffect(e) and Duel.Remove(tc,tc:GetPosition(),REASON_EFFECT+REASON_TEMPORARY)~=0 then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
