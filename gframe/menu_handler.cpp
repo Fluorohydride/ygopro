c7713661e41023c67e258fea060bf8ee9fe3f624
Fluorohydride <argon.sun@gmail.com>
2014-01-01 12:56:44 +0800

    update

--- gframe/menu_handler.cpp
+++ gframe/menu_handler.cpp
@@ -31,27 +31,27 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 				break;
 			}
 			case BUTTON_JOIN_HOST: {
-				struct addrinfo hints, *servinfo;
-				memset(&hints, 0, sizeof(struct addrinfo));
-				hints.ai_family = AF_INET;			/* Allow IPv4 or IPv6 */
-				hints.ai_socktype = SOCK_STREAM;	/* Datagram socket */
-				hints.ai_flags = AI_PASSIVE;		/* For wildcard IP address */
-				hints.ai_protocol = 0;				/* Any protocol */
-				hints.ai_canonname = NULL;
-				hints.ai_addr = NULL;
-				hints.ai_next = NULL;
+//				struct addrinfo hints, *servinfo;
+//				memset(&hints, 0, sizeof(struct addrinfo));
+//				hints.ai_family = AF_INET;			/* Allow IPv4 or IPv6 */
+//				hints.ai_socktype = SOCK_STREAM;	/* Datagram socket */
+//				hints.ai_flags = AI_PASSIVE;		/* For wildcard IP address */
+//				hints.ai_protocol = 0;				/* Any protocol */
+//				hints.ai_canonname = NULL;
+//				hints.ai_addr = NULL;
+//				hints.ai_next = NULL;
 				int status;
 				char hostname[100];
 				char ip[20];
 				const wchar_t* pstr = mainGame->ebJoinIP->getText();
 				BufferIO::CopyWStr(pstr, hostname, 100);
-				if ((status = getaddrinfo(hostname, NULL, &hints, &servinfo)) == -1) {
-					//fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-					//error handling
+//				if ((status = getaddrinfo(hostname, NULL, &hints, &servinfo)) == -1) {
+//					//fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+//					//error handling
 					BufferIO::CopyWStr(pstr, ip, 16);
-				} else
-					inet_ntop(AF_INET, &(((struct sockaddr_in *)servinfo->ai_addr)->sin_addr), ip, 20);
-				freeaddrinfo(servinfo);
+//				} else
+//					inet_ntop(AF_INET, &(((struct sockaddr_in *)servinfo->ai_addr)->sin_addr), ip, 20);
+//				freeaddrinfo(servinfo);
 				unsigned int remote_addr = htonl(inet_addr(ip));
 				unsigned int remote_port = _wtoi(mainGame->ebJoinPort->getText());
 				BufferIO::CopyWStr(pstr, mainGame->gameConf.lastip, 20);
