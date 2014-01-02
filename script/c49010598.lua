0deb045c05c9facb1ad549d5de3d4dda6a447f21
salix5 <salix5@livemail.tw>
2014-01-02 17:16:02 +0800

    fix
    type error

--- script/c49010598.lua
+++ script/c49010598.lua
@@ -21,7 +21,7 @@ end
 function c49010598.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+	if re:GetHandler():IsDestructable() and (re:GetActivateLocation()==LOCATION_EXTRA or re:GetHandler():IsRelateToEffect(re)) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
