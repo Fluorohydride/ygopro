afb3a995a66812de555aa2fe26534edd8ed1065b
sidschingis <sijing@gmx.net>
2014-04-21 20:02:17 +0200

    DRLG/CPL2
    Rest of the cards
    SetCodes used:
    !setcode 0xa2 Stardust
    Counter used:
    !counter 0x95 Utopia Counter
    cards not included:
    Wiretap 34507039
    Mound of the Bound Creator 269012

---
+++ script/c95929069.lua
@@ -0,0 +1,48 @@
+--アイス・ハンド
+function c95929069.initial_effect(c)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(95929069,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCondition(c95929069.condition)
+	e1:SetTarget(c95929069.target)
+	e1:SetOperation(c95929069.operation)
+	c:RegisterEffect(e1)
+end
+function c95929069.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetPreviousControler()==tp and c:IsReason(REASON_DESTROY+REASON_BATTLE) and rp~=tp
+end
+function c95929069.dfilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsDestructable()
+end
+function c95929069.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and c95929069.dfilter(chkc) end
+	if chk==0 then return 
+		Duel.IsExistingTarget(c95929069.dfilter,tp,0,LOCATION_ONFIELD,1,nil)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c95929069.dfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+
+function c95929069.spfilter(c)
+	return c:GetCode()==68535320 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c95929069.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local g=Duel.GetMatchingGroup(c95929069.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+		if Duel.Destroy(tc,REASON_EFFECT)
+		and Duel.SelectYesNo(tp,aux.Stringid(95929069,1))
+		and g:GetCount()>0
+		then
+			g:Select(tp,1,1,nil)
+			Duel.BreakEffect()
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
---
+++ script/c96142517.lua
@@ -0,0 +1,72 @@
+--ＲＵＭ－千死蛮巧
+function c96142517.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCost(c96142517.cost)
+	e1:SetTarget(c96142517.target)
+	e1:SetOperation(c96142517.activate)
+	c:RegisterEffect(e1)
+end
+function c96142517.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.CheckSpecialSummonActivity(tp) end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c96142517.sumlimit)
+	Duel.RegisterEffect(e1,tp)
+end
+function c96142517.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return e:GetHandler()~=se:GetHandler()
+end
+function c96142517.filter1(c,e,tp)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+	and Duel.IsExistingMatchingCard(c96142517.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetRank()+1)
+
+end
+function c96142517.filter2(c,e,tp,rank)
+	return c:GetRank()==rank and (c:IsSetCard(0x1048) or c:IsSetCard(0x1073))
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+end
+function c96142517.filter3(c,e,tp,rank)
+	return c:GetRank()==rank and c:IsType(TYPE_XYZ)
+end
+function c96142517.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c96142517.filter1(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c96142517.filter1,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp)end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local tg=Duel.SelectTarget(tp,c96142517.filter1,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+	e:SetLabel(tg:GetFirst():GetRank())
+	local g=Duel.GetMatchingGroup(c96142517.filter3,tp,LOCATION_GRAVE,LOCATION_GRAVE,tg:GetFirst(),e,tp,tg:GetFirst():GetRank())
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(96142517,1)) then
+		g=g:Select(tp,1,99,nil)
+		tg:Merge(g)
+		local tc=g:GetFirst()
+		while tc do
+			Duel.SetTargetCard(tc)
+			tc=g:GetNext()
+		end
+	end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,tg,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c96142517.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<0 then return end
+	local rank=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c96142517.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,rank+1)
+	local sc=g:GetFirst()
+	if sc then
+		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+		Duel.Overlay(sc,g)
+		Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+		sc:CompleteProcedure()
+	end
+end
