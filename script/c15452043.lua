e2ee22923330c39f6263684371196f47ecd9f480
salix5 <salix5@livemail.tw>
2014-04-24 15:13:53 +0800

    fix: 901
    Card ID are fixed, and an empty script for c17390179 閃光の騎士 is added.

--- script/c15452043.lua
+++ script/c15452043.lua
@@ -1,32 +1,32 @@
 --EMソード・フィッシュ
-function c15452045.initial_effect(c)
+function c15452043.initial_effect(c)
 	--addown
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(15452045,0))
+	e1:SetDescription(aux.Stringid(15452043,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetOperation(c15452045.operation)
+	e1:SetOperation(c15452043.operation)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	--addown
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(15452045,1))
+	e3:SetDescription(aux.Stringid(15452043,1))
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(c15452045.condition)
-	e3:SetOperation(c15452045.operation)
+	e3:SetCondition(c15452043.condition)
+	e3:SetOperation(c15452043.operation)
 	c:RegisterEffect(e3)
 end
-function c15452045.cfilter(c,tp)
+function c15452043.cfilter(c,tp)
 	return c:GetSummonPlayer()==tp
 end
-function c15452045.condition(e,tp,eg,ep,ev,re,r,rp)
-	return not eg:IsContains(e:GetHandler()) and eg:IsExists(c15452045.cfilter,1,nil,tp)
+function c15452043.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsContains(e:GetHandler()) and eg:IsExists(c15452043.cfilter,1,nil,tp)
 end
-function c15452045.operation(e,tp,eg,ep,ev,re,r,rp)
+function c15452043.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
 	local tc=g:GetFirst()
 	while tc do
--- script/c56638325.lua
+++ script/c56638325.lua
@@ -1,5 +1,5 @@
 --星輝士 デルタテロス
-function c5663825.initial_effect(c)
+function c56638325.initial_effect(c)
 	--xyz summon
 	aux.AddXyzProcedure(c,aux.XyzFilterFunction(c,4),3)
 	c:EnableReviveLimit()
@@ -8,80 +8,80 @@ function c5663825.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(c5663825.limcon)
-	e1:SetOperation(c5663825.limop)
+	e1:SetCondition(c56638325.limcon)
+	e1:SetOperation(c56638325.limop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	--destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(5663825,0))
+	e3:SetDescription(aux.Stringid(56638325,0))
 	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetCountLimit(1)
-	e3:SetCost(c5663825.descost)
-	e3:SetTarget(c5663825.destg)
-	e3:SetOperation(c5663825.desop)
+	e3:SetCost(c56638325.descost)
+	e3:SetTarget(c56638325.destg)
+	e3:SetOperation(c56638325.desop)
 	c:RegisterEffect(e3)
 	--spsummon
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(5663825,1))
+	e4:SetDescription(aux.Stringid(56638325,1))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_TO_GRAVE)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e4:SetCondition(c5663825.spcon)
-	e4:SetTarget(c5663825.sptg)
-	e4:SetOperation(c5663825.spop)
+	e4:SetCondition(c56638325.spcon)
+	e4:SetTarget(c56638325.sptg)
+	e4:SetOperation(c56638325.spop)
 	c:RegisterEffect(e4)
 end
-function c5663825.limfilter(c,tp)
+function c56638325.limfilter(c,tp)
 	return c:GetSummonPlayer()==tp
 end
-function c5663825.limcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetOverlayCount()>0 and eg:IsExists(c5663825.limfilter,1,nil,tp)
+function c56638325.limcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetOverlayCount()>0 and eg:IsExists(c56638325.limfilter,1,nil,tp)
 end
-function c5663825.limop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(c5663825.chainlm)
+function c56638325.limop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(c56638325.chainlm)
 end
-function c5663825.chainlm(e,rp,tp)
+function c56638325.chainlm(e,rp,tp)
 	return tp==rp
 end
-function c5663825.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c56638325.descost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c5663825.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c56638325.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c5663825.desop(e,tp,eg,ep,ev,re,r,rp)
+function c56638325.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
-function c5663825.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c56638325.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
-function c5663825.spfilter(c,e,tp)
+function c56638325.spfilter(c,e,tp)
 	return c:IsSetCard(0x9c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c5663825.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c56638325.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c5663825.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c56638325.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
-function c5663825.spop(e,tp,eg,ep,ev,re,r,rp)
+function c56638325.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c5663825.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c56638325.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
