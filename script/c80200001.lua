c07ac150a3c3182870ab07f5b2963e49a6e77ce9
sidschingis <sijing@gmx.net>
2014-05-18 23:56:15 +0200

    new

---
+++ script/c80200001.lua
@@ -0,0 +1,65 @@
+--ＥＭチアモール
+function c80200001.initial_effect(c)
+	--pendulum summon
+	aux.AddPendulumProcedure(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCondition(c80200001.atkcon)
+	e2:SetTarget(c80200001.atktg)
+	e2:SetValue(300)
+	c:RegisterEffect(e2)
+	--attack up/down
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetDescription(aux.Stringid(80200001,0))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1,80200001)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(c80200001.target)
+	e3:SetOperation(c80200001.operation)
+	c:RegisterEffect(e3)
+end
+function c80200001.atkcon(e)
+	return e:GetHandler():GetSequence()==6 or e:GetHandler():GetSequence()==7
+end
+function c80200001.atktg(e,c)
+	return c:IsType(TYPE_PENDULUM)
+end
+function c80200001.filter(c)
+	return c:IsFaceup() and c:GetAttack()~=c:GetBaseAttack()
+end
+function c80200001.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c80200001.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c80200001.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c80200001.filter,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c80200001.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local atk=tc:GetAttack()
+		local batk=tc:GetBaseAttack()
+		if batk==atk then return end
+		local val=1000
+		if batk>atk then
+			val=-1000
+		end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(val)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+	end
+end
