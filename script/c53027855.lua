bfa03db117e7a4233cf28372487a38b10aa4da9a
Steeldarkeagel <steeldarkeagel@live.com>
2014-05-19 14:44:29 +0100

    Update c53027855.lua

--- script/c53027855.lua
+++ script/c53027855.lua
@@ -1,5 +1,5 @@
---炎霊神パイロレクス
-function c35842855.initial_effect(c)
+--風霊神ウィンドローズ
+function c53027855.initial_effect(c)
 	c:EnableReviveLimit()
 	--cannot special summon
 	local e1=Effect.CreateEffect(c)
@@ -13,53 +13,43 @@ function c35842855.initial_effect(c)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_HAND)
-	e2:SetCondition(c35842855.spcon)
+	e2:SetCondition(c53027855.spcon)
 	c:RegisterEffect(e2)
 	--destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(35842855,0))
-	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetDescription(aux.Stringid(53027855,0))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCountLimit(1,35842855)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetTarget(c35842855.destg)
-	e3:SetOperation(c35842855.desop)
+	e3:SetCountLimit(1,53027855)
+	e3:SetTarget(c53027855.destg)
+	e3:SetOperation(c53027855.desop)
 	c:RegisterEffect(e3)
 	--leave
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e4:SetCode(EVENT_LEAVE_FIELD)
-	e4:SetOperation(c35842855.leaveop)
+	e4:SetOperation(c53027855.leaveop)
 	c:RegisterEffect(e4)
 end
-function c35842855.spcon(e,c)
+function c53027855.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and
-		Duel.GetMatchingGroupCount(Card.IsAttribute,c:GetControler(),LOCATION_GRAVE,0,nil,ATTRIBUTE_FIRE)==5
+		Duel.GetMatchingGroupCount(Card.IsAttribute,c:GetControler(),LOCATION_GRAVE,0,nil,ATTRIBUTE_WIND)==5
 end
-function c35842855.filter(c)
-	return c:IsDestructable()
+function c53027855.desfilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsDestructable()
 end
-function c35842855.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c35842855.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c35842855.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c35842855.filter,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,0)
+function c53027855.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(c53027855.desfilter,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
-function c35842855.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)==1 then
-		local atk=tc:GetTextAttack()/2
-		if atk>0 then
-			Duel.Damage(tp,atk,REASON_EFFECT)
-			Duel.Damage(1-tp,atk,REASON_EFFECT)
-		end
-	end
+function c53027855.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c53027855.desfilter,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Destroy(g,REASON_EFFECT)
 end
-function c35842855.leaveop(e,tp,eg,ep,ev,re,r,rp)
+function c53027855.leaveop(e,tp,eg,ep,ev,re,r,rp)
 	local effp=e:GetHandler():GetPreviousControler()
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -68,13 +58,13 @@ function c35842855.leaveop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTargetRange(1,0)
 	if Duel.GetTurnPlayer()==effp then
 		e1:SetLabel(Duel.GetTurnCount())
-		e1:SetCondition(c35842855.skipcon)
+		e1:SetCondition(c53027855.skipcon)
 		e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
 	else
 		e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
 	end
 	Duel.RegisterEffect(e1,effp)
 end
-function c35842855.skipcon(e)
+function c53027855.skipcon(e)
 	return Duel.GetTurnCount()~=e:GetLabel()
 end
