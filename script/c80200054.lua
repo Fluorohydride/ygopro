07c4c5eb84c1da9fcf3e83c2761f64eb32b271cd
sidschingis <sijing@gmx.net>
2014-05-20 18:55:44 +0200

    new

---
+++ script/c80200054.lua
@@ -0,0 +1,123 @@
+--星輝士 トライヴェール
+function c80200054.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,aux.XyzFilterFunctionF(c,aux.FilterBoolFunction(Card.IsSetCard,0x9c),4),3)
+	c:EnableReviveLimit()
+	--Summon Limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetOperation(c80200054.sumop)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetCondition(c80200054.sumcon)
+	c:RegisterEffect(e2)
+	--to Hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(80200054,0))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(c80200054.thcon)
+	e3:SetTarget(c80200054.thtg)
+	e3:SetOperation(c80200054.thop)
+	c:RegisterEffect(e3)
+	--handdes
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(80200054,1))
+	e4:SetCategory(CATEGORY_TOGRAVE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCost(c80200054.hdcost)
+	e4:SetTarget(c80200054.hdtg)
+	e4:SetOperation(c80200054.hdop)
+	c:RegisterEffect(e4)
+	--spsummon
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(80200054,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e5:SetCondition(c80200054.spcon)
+	e5:SetTarget(c80200054.sptg)
+	e5:SetOperation(c80200054.spop)
+	c:RegisterEffect(e5)
+	if not c80200054.global_check then
+		c80200054.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetOperation(c80200054.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function c80200054.sumop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,80200054,RESET_PHASE+PHASE_END,0,1)
+end
+function c80200054.sumcon(e)
+	return Duel.GetFlagEffect(sump,80200054)==0
+end
+function c80200054.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		local sump=tc:GetSummonPlayer()
+		if not tc:IsSetCard(0x9c) and Duel.GetFlagEffect(sump,80200054)==0 then
+			Duel.RegisterFlagEffect(sump,80200054,RESET_PHASE+PHASE_END,0,1)
+		end
+		tc=eg:GetNext()
+	end
+end
+function c80200054.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_XYZ
+end
+function c80200054.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
+end
+function c80200054.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+end
+function c80200054.hdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function c80200054.hdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)~=0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,1-tp,LOCATION_HAND)
+end
+function c80200054.hdop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
+	if g:GetCount()==0 then return end
+	local sg=g:RandomSelect(1-tp,1)
+	Duel.SendtoGrave(sg,REASON_EFFECT)
+end
+function c80200054.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetOverlayCount()>0
+end
+function c80200054.spfilter(c,e,tp)
+	return c:IsSetCard(0x9c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c80200054.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:GetLocation()==LOCATION_GRAVE and chkc:GetControler()==tp and chkc:IsSetCard(0x9c)
+		and chkc:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c80200054.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c80200054.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c80200054.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
