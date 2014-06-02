7582c80a4c652e11d94928b070e8e38ad0c873fc
sidschingis <sijing@gmx.net>
2014-06-02 18:57:07 +0200

    fix

--- script/c80200001.lua
+++ script/c80200001.lua
@@ -40,9 +40,9 @@ function c80200001.filter(c)
 end
 function c80200001.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c80200001.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c80200001.filter,tp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(c80200001.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c80200001.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,c80200001.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function c80200001.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -51,9 +51,9 @@ function c80200001.operation(e,tp,eg,ep,ev,re,r,rp)
 		local atk=tc:GetAttack()
 		local batk=tc:GetBaseAttack()
 		if batk==atk then return end
-		local val=1000
-		if batk<atk then
-			val=-1000
+		local val=-1000
+		if atk>batk then
+			val=1000
 		end
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
