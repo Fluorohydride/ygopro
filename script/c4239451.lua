31e251ac4ce893fa77d07c124b7e880496bc0191
VanillaSalt <vanillasalt5197@gmail.com>
2014-06-10 08:32:03 +0900

    fix

--- script/c4239451.lua
+++ script/c4239451.lua
@@ -68,13 +68,13 @@ end
 function c4239451.adtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) end
 	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	Duel.SelectTarget(tp,nil,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function c4239451.adop2(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.ChangePosition(tc,POS_FACEUP_DEFENCE,0,POS_FACEUP_ATTACK,0)
+	if tc:IsRelateToEffect(e)
+		and Duel.ChangePosition(tc,POS_FACEUP_DEFENCE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)~=0 then
 		local atk=tc:GetAttack()
 		local def=tc:GetDefence()
 		local e1=Effect.CreateEffect(e:GetHandler())
