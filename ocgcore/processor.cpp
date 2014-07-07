4b6035cb90e6aef59da901b04072de6ca8da06dd
sidschingis <sijing@gmx.net>
2014-07-07 20:01:33 +0200

    format

--- ocgcore/processor.cpp
+++ ocgcore/processor.cpp
@@ -484,7 +484,7 @@ int32 field::process() {
 			        || (attacker->current.position & POS_FACEDOWN)
 			        || ((attacker->current.position & POS_DEFENCE) && !(attacker->is_affected_by_effect(EFFECT_DEFENCE_ATTACK)))
 			        || attacker->is_affected_by_effect(EFFECT_ATTACK_DISABLED)		        
-					|| attacker->is_affected_by_effect(EFFECT_CANNOT_ATTACK)
+			        || attacker->is_affected_by_effect(EFFECT_CANNOT_ATTACK)
 			        || !attacker->is_affect_by_effect(core.reason_effect)) {
 				returns.ivalue[0] = 0;
 				pduel->lua->add_param(returns.ivalue[0], PARAM_TYPE_BOOLEAN);
