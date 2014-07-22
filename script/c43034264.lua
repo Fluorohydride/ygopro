1db66f8af2668d0160cb9e860c742d0aeb57ff78
VanillaSalt <vanillasalt5197@gmail.com>
2014-07-22 23:08:13 +0900

    new

---
+++ script/c43034264.lua
@@ -0,0 +1,24 @@
+--機殻の要塞
+function c43034264.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--extra summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e2:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xaa))
+	c:RegisterEffect(e2)
+	--cannot disable summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xaa))
+	c:RegisterEffect(e3)
+end
