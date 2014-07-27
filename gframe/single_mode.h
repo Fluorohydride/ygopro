0cd0ae88c2490c9c526744dd954e62a2b566f85f
sidschingis <sijing@gmx.net>
2014-07-27 20:28:36 +0200

    Update single_mode.h

--- gframe/single_mode.h
+++ gframe/single_mode.h
@@ -23,10 +23,10 @@ public:
 	static bool SinglePlayAnalyze(char* msg, unsigned int len);
 	
 	static void SinglePlayRefresh(int flag = 0x781fff);
-	static void SinglePlayRefreshHand(int player, int flag = 0x781fff);
-	static void SinglePlayRefreshGrave(int player, int flag = 0x781fff);
-	static void SinglePlayRefreshDeck(int player, int flag = 0x781fff);
-	static void SinglePlayRefreshExtra(int player, int flag = 0x781fff);
+	static void SinglePlayRefreshHand(int player, int flag = 0x181fff);
+	static void SinglePlayRefreshGrave(int player, int flag = 0x181fff);
+	static void SinglePlayRefreshDeck(int player, int flag = 0x181fff);
+	static void SinglePlayRefreshExtra(int player, int flag = 0x181fff);
 	static void SinglePlayRefreshSingle(int player, int location, int sequence, int flag = 0x781fff);
 	static void SinglePlayReload();
 
