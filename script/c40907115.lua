88840498825affe2b5c044caf548f3340ec430bd
VanillaSalt <vanillasalt5197@gmail.com>
2014-10-13 15:11:08 +0900

    fix

--- script/c40907115.lua
+++ script/c40907115.lua
@@ -54,7 +54,7 @@ function c40907115.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c40907115.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
--- script/c75457624.lua
+++ script/c75457624.lua
@@ -28,6 +28,7 @@ function c75457624.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c75457624.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c75457624.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
