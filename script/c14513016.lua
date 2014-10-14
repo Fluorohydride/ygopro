821dd91a7f78e07839e7afb6bf513e2ffaa9c03e
VanillaSalt <vanillasalt5197@gmail.com>
2014-10-14 09:37:05 +0900

    fix

--- script/c14513016.lua
+++ script/c14513016.lua
@@ -38,7 +38,7 @@ function c14513016.limcon(e)
 	return Duel.GetFlagEffect(e:GetHandlerPlayer(),14513016)~=0
 end
 function c14513016.sumop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,14513016)~=0 then return end
+	if Duel.GetFlagEffect(tp,14513017)~=0 then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
@@ -46,5 +46,5 @@ function c14513016.sumop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xb5))
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	Duel.RegisterFlagEffect(tp,14513016,RESET_PHASE+PHASE_END,0,1)
+	Duel.RegisterFlagEffect(tp,14513017,RESET_PHASE+PHASE_END,0,1)
 end
