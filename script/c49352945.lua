d90844c32b345f78ef062885402a3a0440a464df
Tianchenglipu <670204195@qq.com>
2014-11-07 02:35:36 +0800

    Update c49352945.lua
    p/3395207479
    Bug noticed in this post.

--- script/c49352945.lua
+++ script/c49352945.lua
@@ -61,8 +61,7 @@ function c49352945.splimit(e,se,sp,st)
 end
 function c49352945.spfilter(c,code)
 	if c:GetCode()~=code then return false end
-	if c:IsType(TYPE_FUSION) then return c:IsAbleToExtraAsCost()
-	else return c:IsAbleToDeckAsCost() end
+	return c:IsAbleToDeckAsCost() or c:IsAbleToExtraAsCost()
 end
 function c49352945.spcon(e,c)
 	if c==nil then return true end 
