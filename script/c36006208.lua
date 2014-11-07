6169ac1890ac080a6fe658984c6c480b72b716d1
VanillaSalt <vanillasalt5197@gmail.com>
2014-11-07 21:02:15 +0900

    fix

--- script/c36006208.lua
+++ script/c36006208.lua
@@ -1,77 +1,76 @@
 --Fire Lake of the Burning Abyss
 function c36006208.initial_effect(c)
-    --Activate
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DESTROY)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetHintTiming(0,0x1e0)
-    e1:SetCost(c36006208.cost)
-    e1:SetTarget(c36006208.target)
-    e1:SetOperation(c36006208.activate)
-    c:RegisterEffect(e1)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,0x1e0)
+	e1:SetCost(c36006208.cost)
+	e1:SetTarget(c36006208.target)
+	e1:SetOperation(c36006208.activate)
+	c:RegisterEffect(e1)
 end
 function c36006208.filter(c)
-    return c:IsSetCard(0xb1) and c:IsFaceup() and c:IsAbleToGrave()
+	return c:IsFaceup() and c:IsSetCard(0xb1) and c:IsAbleToGraveAsCost()
 end
 function c36006208.costfilter(c,rg,dg)
-    if not rg:IsContains(c) then return false end
-    local a=0
-    if dg:IsContains(c) then a=1 end
-    if c:GetEquipCount()==0 then return rg:IsExists(c36006208.costfilter2,1,c,a,dg) end
-    local eg=c:GetEquipGroup()
-    local tc=eg:GetFirst()
-    while tc do
-        if dg:IsContains(tc) then a=a+1 end
-        tc=eg:GetNext()
-    end
-    return rg:IsExists(c36006208.costfilter2,1,c,a,dg)
+	local a=0
+	if dg:IsContains(c) then a=1 end
+	if c:GetEquipCount()==0 then return rg:IsExists(c36006208.costfilter2,1,c,a,dg) end
+	local eg=c:GetEquipGroup()
+	local tc=eg:GetFirst()
+	while tc do
+		if dg:IsContains(tc) then a=a+1 end
+		tc=eg:GetNext()
+	end
+	return rg:IsExists(c36006208.costfilter2,1,c,a,dg)
 end
 function c36006208.costfilter2(c,a,dg)
-    if dg:IsContains(c) then a=a+1 end
-    if c:GetEquipCount()==0 then return dg:GetCount()-a>=1 end
-    local eg=c:GetEquipGroup()
-    local tc=eg:GetFirst()
-    while tc do
-        if dg:IsContains(tc) then a=a+1 end
-        tc=eg:GetNext()
-    end
-    return dg:GetCount()-a>=1
+	if dg:IsContains(c) then a=a+1 end
+	if c:GetEquipCount()==0 then return dg:GetCount()-a>=1 end
+	local eg=c:GetEquipGroup()
+	local tc=eg:GetFirst()
+	while tc do
+		if dg:IsContains(tc) then a=a+1 end
+		tc=eg:GetNext()
+	end
+	return dg:GetCount()-a>=1
 end
 function c36006208.tgfilter(c,e)
-    return c:IsDestructable() and c:IsCanBeEffectTarget(e)
+	return c:IsDestructable() and c:IsCanBeEffectTarget(e)
 end
 function c36006208.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    e:SetLabel(1)
-    return true
+	e:SetLabel(1)
+	return true
 end
 function c36006208.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chk==0 then
-        if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
-        if e:GetLabel()==1 then
-            e:SetLabel(0)
-            local rg=Duel.GetMatchingGroup(c36006208.filter,tp,LOCATION_MZONE,0,nil)
-            local dg=Duel.GetMatchingGroup(c36006208.tgfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler(),e)
-            return Duel.IsExistingMatchingCard(c36006208.costfilter,tp,LOCATION_MZONE,0,2,nil,rg,dg)
-        else
-            return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler())
-        end
-    end
-    if e:GetLabel()==1 then
-        e:SetLabel(0)
-        local rg=Duel.GetMatchingGroup(c36006208.filter,tp,LOCATION_MZONE,0,nil)
-        local dg=Duel.GetMatchingGroup(c36006208.tgfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler(),e)
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-        local sg=Duel.SelectMatchingCard(tp,c36006208.costfilter,tp,LOCATION_MZONE,0,2,2,nil,rg,dg)
-        Duel.SendtoGrave(sg,REASON_COST)
-    end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,3,e:GetHandler())
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+	if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
+	if chk==0 then
+		if e:GetLabel()==1 then
+			e:SetLabel(0)
+			local rg=Duel.GetMatchingGroup(c36006208.filter,tp,LOCATION_MZONE,0,nil)
+			local dg=Duel.GetMatchingGroup(c36006208.tgfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler(),e)
+			return rg:IsExists(c36006208.costfilter,2,nil,rg,dg)
+		else
+			return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler())
+		end
+	end
+	if e:GetLabel()==1 then
+		e:SetLabel(0)
+		local rg=Duel.GetMatchingGroup(c36006208.filter,tp,LOCATION_MZONE,0,nil)
+		local dg=Duel.GetMatchingGroup(c36006208.tgfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler(),e)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local sg=rg:FilterSelect(tp,c36006208.costfilter,2,2,nil,rg,dg)
+		Duel.SendtoGrave(sg,REASON_COST)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,3,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end 
 function c36006208.activate(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-    local sg=g:Filter(Card.IsRelateToEffect,nil,e)
-    Duel.Destroy(sg,REASON_EFFECT)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	Duel.Destroy(sg,REASON_EFFECT)
 end
