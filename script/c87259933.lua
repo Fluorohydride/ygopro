6f7caed33375e401757a2cb4529eef9092f27cc1
VanillaSalt <vanillasalt5197@gmail.com>
2014-11-18 20:40:14 +0900

    fix

--- script/c87259933.lua
+++ script/c87259933.lua
@@ -111,7 +111,7 @@ function c87259933.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g1,2,0,0)
 end
 function c87259933.thop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 	end
