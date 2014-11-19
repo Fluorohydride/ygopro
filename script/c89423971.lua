de0cd37e9bbf5e2754bee4111aa1197664b9731d
Lsty <281606174@qq.com>
2014-11-19 08:25:57 +0800

    Update c89423971.lua

--- script/c89423971.lua
+++ script/c89423971.lua
@@ -1,6 +1,6 @@
 --インフェルノイド・アスタロス
 function c89423971.initial_effect(c)
-	c:EnableReviveLimit()
+	c:SetStatus(STATUS_UNSUMMONABLE_CARD,true)
 	--special summon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
