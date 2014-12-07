c14a1943a01b8693595324e42c8f28b0ed7179a3
DailyShana <p2357111317192329@gmail.com>
2014-12-07 14:19:45 +0800

    Update c37675907.lua

--- script/c37675907.lua
+++ script/c37675907.lua
@@ -3,41 +3,47 @@ function c37675907.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
 	c:EnableReviveLimit()
-	--splimit
-	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c37675907.regcon)
-	e1:SetOperation(c37675907.regop)
-	c:RegisterEffect(e1)
 	--lv
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(37675907,0))
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1)
-	e2:SetCost(c37675907.tgcost)
-	e2:SetTarget(c37675907.tgtg)
-	e2:SetOperation(c37675907.tgop)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(37675907,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(c37675907.tgcost)
+	e1:SetTarget(c37675907.tgtg)
+	e1:SetOperation(c37675907.tgop)
+	c:RegisterEffect(e1)
 	--spsummon
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(37675907,1))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetHintTiming(0,0x1c0+TIMING_MAIN_END)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(c37675907.spcon)
-	e3:SetTarget(c37675907.sptg)
-	e3:SetOperation(c37675907.spop)
-	c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(37675907,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,0x1c0+TIMING_MAIN_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c37675907.spcon)
+	e2:SetTarget(c37675907.sptg)
+	e2:SetOperation(c37675907.spop)
+	c:RegisterEffect(e2)
+	if not c37675907.global_check then
+		c37675907.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetOperation(c37675907.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
 end
-function c37675907.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
-end
-function c37675907.regop(e,tp,eg,ep,ev,re,r,rp)
+function c37675907.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	local p1=false
+	local p2=false
+	while tc do
+		if tc:IsCode(37675907) and tc:GetSummonType()==SUMMON_TYPE_SYNCHRO then
+			if tc:GetSummonPlayer()==0 then p1=true else p2=true end
+		end
+		tc=eg:GetNext()
+	end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
@@ -45,7 +51,8 @@ function c37675907.regop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTargetRange(1,0)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTarget(c37675907.splimit)
-	Duel.RegisterEffect(e1,tp)
+	if p1 then Duel.RegisterEffect(e1,0) end
+	if p2 then Duel.RegisterEffect(e1,1) end
 end
 function c37675907.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return c:IsCode(37675907) and bit.band(sumtype,SUMMON_TYPE_SYNCHRO)==SUMMON_TYPE_SYNCHRO
