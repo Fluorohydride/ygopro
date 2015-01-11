74da9e76555f8b6d17f3c2d317608331187a5def
sidschingis <sijing@gmx.net>
2015-01-11 18:11:17 +0100

    fix
    fixed being able to add Dance Princess

--- script/c52738610.lua
+++ script/c52738610.lua
@@ -47,7 +47,7 @@ function c52738610.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(r,REASON_EFFECT)~=0
 end
 function c52738610.thfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xb4) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+	return c:IsFaceup() and c:IsSetCard(0xb4) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and not c:IsCode(52738610)
 end
 function c52738610.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and c52738610.thfilter(chkc) end
