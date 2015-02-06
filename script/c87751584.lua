e83aad483a43b891c29cd5485e2f2f5fc8cf92a4
salix5 <salix5@livemail.tw>
2015-02-06 12:10:53 +0800

    fix

--- script/c87751584.lua
+++ script/c87751584.lua
@@ -24,6 +24,7 @@ function c87751584.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c1,c2,c3=Duel.TossCoin(tp,3)
 	local ct=c1+c2+c3
 	if ct==0 then return end
+	if ct>g:GetCount() then ct=g:GetCount() end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local dg=g:Select(tp,ct,ct,nil)
 	Duel.HintSelection(dg)
