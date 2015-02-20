e8666bbabaa2ad7df3c9a305453ec2f2340abea3
salix5 <salix5@livemail.tw>
2015-02-21 00:35:50 +0800

    fix

--- script/c43262273.lua
+++ script/c43262273.lua
@@ -25,33 +25,43 @@ function c43262273.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,1-tp,LOCATION_HAND)
 end
 function c43262273.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g0=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	Duel.ConfirmCards(tp,g0)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil)
 	if g:GetCount()>0 then
-		Duel.ConfirmCards(tp,g)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 		local sg=g:Select(tp,1,1,nil)
-		Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
-		Duel.ShuffleHand(1-tp)
+		local tc=sg:GetFirst()
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+		tc:RegisterFlagEffect(43262273,RESET_EVENT+0x1fe0000,0,1)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetRange(LOCATION_REMOVED)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetCountLimit(1)
 		if Duel.GetTurnPlayer()==1-tp and Duel.GetCurrentPhase()==PHASE_END then
 			e1:SetLabel(Duel.GetTurnCount())
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,3)
+			e1:SetReset(RESET_PHASE+PHASE_END,3)
 		else
 			e1:SetLabel(0)
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,2)
+			e1:SetReset(RESET_PHASE+PHASE_END,2)
 		end
+		e1:SetLabelObject(tc)
 		e1:SetCondition(c43262273.retcon)
 		e1:SetOperation(c43262273.retop)
-		sg:GetFirst():RegisterEffect(e1)
+		Duel.RegisterEffect(e1,tp)
 	end
+	Duel.ShuffleHand(1-tp)
 end
 function c43262273.retcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and Duel.GetTurnCount()~=e:GetLabel()
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffect(43262273)==0 then
+		e:Reset()
+		return false
+	else
+		return Duel.GetTurnPlayer()==1-tp and Duel.GetTurnCount()~=e:GetLabel()
+	end
 end
 function c43262273.retop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+	local tc=e:GetLabelObject()
+	Duel.SendtoHand(tc,1-tp,REASON_EFFECT)
 end
