3b1a76f6da0b9fecb7c193aa3e6d81919cb0bd36
DailyShana <p2357111317192329@gmail.com>
2015-02-21 16:20:04 +0800

    fix

--- script/c82044279.lua
+++ script/c82044279.lua
@@ -20,6 +20,14 @@ function c82044279.initial_effect(c)
 	e2:SetDescription(aux.Stringid(82044279,1))
 	e2:SetCondition(c82044279.condition2)
 	c:RegisterEffect(e2)
+	--atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(c82044279.atkcon)
+	e3:SetOperation(c82044279.atkop)
+	c:RegisterEffect(e3)
 end
 function c82044279.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -39,24 +47,40 @@ function c82044279.condition2(e,tp,eg,ep,ev,re,r,rp)
 end
 function c82044279.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
 function c82044279.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	Duel.NegateActivation(ev)
 	local rc=re:GetHandler()
-	if not rc:IsRelateToEffect(re) then return end
-	if Duel.Destroy(rc,REASON_EFFECT)~=0 and c:IsRelateToEffect(e) and c:IsFaceup() then
-		local atk=rc:GetTextAttack()
-		if atk<0 then atk=0 end
+	if rc:IsRelateToEffect(re) then 
+		Duel.Destroy(rc,REASON_EFFECT)
+	end
+end
+function c82044279.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(r,REASON_EFFECT)~=0 and re and re:GetHandler()==e:GetHandler()
+		and eg:IsExists(Card.IsType,1,nil,TYPE_MONSTER)
+end
+function c82044279.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(Card.IsType,nil,TYPE_MONSTER)
+	local c=e:GetHandler()
+	local atk=0
+	local tc=g:GetFirst()
+	while tc do
+		if tc:GetBaseAttack()>0 then
+			atk=atk+tc:GetBaseAttack()
+		end
+		tc=g:GetNext()
+	end
+	if atk>0 then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
 		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+RESET_END)
 		c:RegisterEffect(e1)
 	end
 end
