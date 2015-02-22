961ab637622aa7503e0299d7166fdea04cbdb7a6
salix5 <salix5@livemail.tw>
2015-02-22 19:37:07 +0800

    fix

--- script/c77901552.lua
+++ script/c77901552.lua
@@ -30,7 +30,7 @@ function c77901552.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)
-	e1:SetReset(RESET_EVENT+0xff0000+RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
 	c:RegisterEffect(e1)
 end
 function c77901552.spfilter(c,e,tp)
