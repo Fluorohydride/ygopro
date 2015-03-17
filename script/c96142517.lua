ea31a259ef7b122342ab82c0263bac2115b7662e
Tianchenglipu <670204195@qq.com>
2015-03-17 19:36:32 +0800

    Update c96142517.lua
    对了，效果处理时目标都不在墓地的话，这个效果不能处理-。-不过如果还剩有目标，剩多少就叠多少

--- script/c96142517.lua
+++ script/c96142517.lua
@@ -80,9 +80,10 @@ function c96142517.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c96142517.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local rk=e:GetLabel()
 	local mg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	local rg=mg:Filter(Card.IsRelateToEffect,nil,e)
+	if rg:GetCount()==0 then return end
+	local rk=e:GetLabel()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c96142517.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,rk+1,mg)
 	local sc=g:GetFirst()
