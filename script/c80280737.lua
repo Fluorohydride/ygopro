8eb7985bf28111ab3918d72fb116d973a1ac79b8
Tianchenglipu <670204195@qq.com>
2015-03-31 21:17:26 +0800

    Update c80280737.lua

--- script/c80280737.lua
+++ script/c80280737.lua
@@ -18,12 +18,12 @@ function c80280737.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	return true
 end
 function c80280737.filter1(c,e,tp)
-	local code=c:GetCode()
-	local tcode=c80280737.list[code]
-	return tcode and c:IsType(TYPE_SYNCHRO) and Duel.IsExistingMatchingCard(c80280737.filter2,tp,LOCATION_DECK,0,1,nil,tcode,e,tp)
+	return c:IsType(TYPE_SYNCHRO) and Duel.IsExistingMatchingCard(c80280737.filter2,tp,LOCATION_DECK,0,1,nil,c:GetCode(),e,tp)
 end
-function c80280737.filter2(c,tcode,e,tp)
-	return c:IsCode(tcode) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+function c80280737.filter2(c,mcode,e,tp)
+	local m=_G["c"..c:GetCode()]
+	local code=m.assault_protocode
+	return code==mcode and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function c80280737.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
@@ -33,15 +33,14 @@ function c80280737.target(e,tp,eg,ep,ev,re,r,rp,chk)
 			and Duel.CheckReleaseGroup(tp,c80280737.filter1,1,nil,e,tp)
 	end
 	local rg=Duel.SelectReleaseGroup(tp,c80280737.filter1,1,1,nil,e,tp)
-	e:SetLabel(rg:GetFirst():GetCode())
+	e:SetLabel(rg:GetFirst():GetCode)
 	Duel.Release(rg,REASON_COST)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c80280737.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local code=e:GetLabel()
-	local tcode=c80280737.list[code]
-	local tc=Duel.GetFirstMatchingCard(c80280737.filter2,tp,LOCATION_DECK,0,nil,tcode,e,tp)
+	local tc=Duel.GetFirstMatchingCard(c80280737.filter2,tp,LOCATION_DECK,0,nil,code,e,tp)
 	if tc and Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP_ATTACK)>0 then
 		tc:CompleteProcedure()
 	end
