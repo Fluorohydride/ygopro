b8702845bc44548f5f3638ba0268461b2f504b23
salix5 <salix5@livemail.tw>
2015-04-08 22:47:31 +0800

    fix

--- script/c42551040.lua
+++ script/c42551040.lua
@@ -35,14 +35,12 @@ function c42551040.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c42551040.posop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		if tc:GetDefence()>0 and Duel.ChangePosition(tc,POS_FACEUP_DEFENCE)~=0 then
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_SET_DEFENCE_FINAL)
-			e1:SetValue(0)
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e1)
-		end
+	if tc:IsRelateToEffect(e) and Duel.ChangePosition(tc,POS_FACEUP_DEFENCE)~=0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_DEFENCE_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
 	end
 end
