e6a7c63c77813993614da1682168168fae03c40a
DailyShana <p2357111317192329@gmail.com>
2015-05-01 14:11:30 +0800

    errata

--- script/c65458948.lua
+++ script/c65458948.lua
@@ -1,21 +1,13 @@
 --トゥーン・マーメイド
 function c65458948.initial_effect(c)
 	c:EnableReviveLimit()
-	--cannot special summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(c65458948.splimit)
-	c:RegisterEffect(e1)
-	--special summon
+	--special summon rule
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetCondition(c65458948.spcon)
-	e2:SetOperation(c65458948.spop)
 	c:RegisterEffect(e2)
 	--destroy
 	local e3=Effect.CreateEffect(c)
@@ -56,32 +48,14 @@ function c65458948.initial_effect(c)
 	e8:SetOperation(c65458948.atop)
 	c:RegisterEffect(e8)
 end
-function c65458948.splimit(e,se,sp,st,spos,tgp)
-	return Duel.IsExistingMatchingCard(c65458948.cfilter,tgp,LOCATION_ONFIELD,0,1,nil)
-end
 function c65458948.cfilter(c)
 	return c:IsFaceup() and c:IsCode(15259703)
 end
 function c65458948.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	if not Duel.IsExistingMatchingCard(c65458948.cfilter,tp,LOCATION_ONFIELD,0,1,nil) then return false end
-	local lv=c:GetLevel()
-	if lv<5 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	elseif lv<7 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and Duel.CheckReleaseGroup(tp,nil,1,nil)
-	else return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2 and Duel.CheckReleaseGroup(tp,nil,2,nil) end
-end
-function c65458948.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local lv=c:GetLevel()
-	local tp=c:GetControler()
-	if lv<5 then 
-	elseif lv<7 then
-		local g=Duel.SelectReleaseGroup(tp,nil,1,1,nil)
-		Duel.Release(g,REASON_COST)
-	else
-		local g=Duel.SelectReleaseGroup(tp,nil,2,2,nil)
-		Duel.Release(g,REASON_COST)
-	end
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c65458948.cfilter,tp,LOCATION_ONFIELD,0,1,nil) 
 end
 function c65458948.sfilter(c)
 	return c:IsReason(REASON_DESTROY) and c:GetPreviousCodeOnField()==15259703 and c:IsPreviousLocation(LOCATION_ONFIELD)
