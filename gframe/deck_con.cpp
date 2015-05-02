35be9764a455379cb3416a9b0265d7023c29ca70
Kelypot <Kelypot@users.noreply.github.com>
2015-05-02 12:24:51 +0800

    Startup Parameters

--- gframe/deck_con.cpp
+++ gframe/deck_con.cpp
@@ -77,9 +77,6 @@ bool DeckBuilder::OnEvent(const irr::SEvent& event) {
 				if(mainGame->cbDBDecks->getSelected() != -1) {
 					BufferIO::CopyWStr(mainGame->cbDBDecks->getItem(mainGame->cbDBDecks->getSelected()), mainGame->gameConf.lastdeck, 64);
 				}
-				if(exit_on_return)
-					mainGame->device->closeDevice();
-				break;
 			}
 			case BUTTON_EFFECT_FILTER: {
 				mainGame->PopupElement(mainGame->wCategories);
