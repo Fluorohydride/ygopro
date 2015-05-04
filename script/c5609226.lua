20beeadf51bbc8b9de82d8584ddf241540f1fe8b
DailyShana <p2357111317192329@gmail.com>
2015-05-04 14:58:21 +0800

    fix

--- script/c5609226.lua
+++ script/c5609226.lua
@@ -20,7 +20,7 @@ function c5609226.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c5609226.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
