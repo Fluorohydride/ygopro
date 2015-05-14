fde5826ffe0550e415580ef1a15456a1cf190bc4
nekrozar <unyuho.buster@gmail.com>
2015-05-14 15:53:38 +0900

    fix

--- script/c59281922.lua
+++ script/c59281922.lua
@@ -73,7 +73,7 @@ function c59281922.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c59281922.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
