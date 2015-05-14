638de5b31ad081dbda5b65c3079fb54625647913
DailyShana <p2357111317192329@gmail.com>
2015-05-14 11:54:37 +0800

    hint

--- script/c44771289.lua
+++ script/c44771289.lua
@@ -2,6 +2,7 @@
 function c44771289.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(44771289,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -10,6 +11,7 @@ function c44771289.initial_effect(c)
 	c:RegisterEffect(e1)
 	--atk & def
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(44771289,1))
 	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
