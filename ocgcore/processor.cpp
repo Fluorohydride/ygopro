ad3122e01d6ff4cf6913733f5d8736492b159658
DailyShana <p2357111317192329@gmail.com>
2015-07-04 10:46:15 +0800

    refresh location info when adjust instant

--- ocgcore/processor.cpp
+++ ocgcore/processor.cpp
@@ -4862,6 +4862,7 @@ int32 field::break_effect() {
 void field::adjust_instant() {
 	adjust_disable_check_list();
 	adjust_self_destroy_set();
+	refresh_location_info_instant();
 }
 void field::adjust_all() {
 	core.readjust_map.clear();
