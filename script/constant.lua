e1388b03d0358849769270efd816e189723373e5
Tianchenglipu <670204195@qq.com>
2015-07-08 04:42:43 +0800

    Update constant.lua

--- script/constant.lua
+++ script/constant.lua
@@ -268,6 +268,7 @@ EFFECT_FLAG_NAGA			=0x10000000 --神卡纳迦！
 EFFECT_FLAG_COF				=0x20000000 --邪恶的仪式
 EFFECT_FLAG_CVAL_CHECK		=0x40000000	--以卡为COST的诱发效果需要使用
 EFFECT_FLAG_IMMEDIATELY_APPLY	=0x80000000	--卡在发动时效果就立即适用（卡通王國）
+EFFECT_FLAG_NON_EFFECT		=0x100000000	--不当作效果（兼带不能无效与复制；用于灵摆召唤手续）
 --========== Codes ==========	--对永续性效果表示效果类型 EFFECT开头，对诱发型效果表示触发效果的事件/时点 EVENT开头
 EFFECT_IMMUNE_EFFECT			=1		--效果免疫
 EFFECT_DISABLE					=2		--效果无效（技能抽取）
