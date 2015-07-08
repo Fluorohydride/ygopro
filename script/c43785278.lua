ce31abdac20ca8f8434a4f12125e49d783a2ea00
DailyShana <p2357111317192329@gmail.com>
2015-07-08 12:10:19 +0800

    fix

--- script/c43785278.lua
+++ script/c43785278.lua
@@ -35,6 +35,7 @@ function c43785278.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c43785278.desop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.Destroy(tc,REASON_EFFECT)
