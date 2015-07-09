d54a457301d5614e56de424ed66a6ea9e298a5c2
DailyShana <p2357111317192329@gmail.com>
2015-07-09 18:56:12 +0800

    fix

--- ocgcore/processor.cpp
+++ ocgcore/processor.cpp
@@ -1748,9 +1748,11 @@ int32 field::process_point_event(int16 step, int32 skip_trigger, int32 skip_free
 				clit->triggering_sequence = peffect->handler->current.sequence;
 			}
 			uint8 tp = clit->triggering_player;
+			effect* preason_effect = peffect->handler->current.reason_effect;
 			bool act = true;
 			if(peffect->is_chainable(tp) && peffect->is_activateable(tp, clit->evt, TRUE)
-			        && ((peffect->code == EVENT_FLIP) || (clit->triggering_location & 0x3) || !(peffect->handler->current.location & 0x3) || peffect->handler->is_status(STATUS_IS_PUBLIC))) {
+			        && (((peffect->code == EVENT_FLIP) && preason_effect && (preason_effect->flag & EFFECT_FLAG_EHREN)) || (clit->triggering_location & 0x3) 
+						|| !(peffect->handler->current.location & 0x3) || peffect->handler->is_status(STATUS_IS_PUBLIC))) {
 				if(peffect->flag & EFFECT_FLAG_CHAIN_UNIQUE) {
 					if(tp == infos.turn_player) {
 						for(auto tpit = core.tpchain.begin(); tpit != core.tpchain.end(); ++tpit) {
@@ -1850,9 +1852,10 @@ int32 field::process_point_event(int16 step, int32 skip_trigger, int32 skip_free
 		auto clit = core.new_ochain_s.begin();
 		effect* peffect = clit->triggering_effect;
 		uint8 tp = clit->triggering_player;
+		effect* preason_effect = peffect->handler->current.reason_effect;
 		bool act = true;
 		if(peffect->is_chainable(tp) && peffect->is_activateable(tp, clit->evt, TRUE)
-		        && ((peffect->code == EVENT_FLIP) || (clit->triggering_location & 0x3)
+		        && (((peffect->code == EVENT_FLIP) && preason_effect && (preason_effect->flag & EFFECT_FLAG_EHREN)) || (clit->triggering_location & 0x3)
 		            || !(peffect->handler->current.location & 0x3) || peffect->handler->is_status(STATUS_IS_PUBLIC))) {
 			if(!(peffect->flag & EFFECT_FLAG_FIELD_ONLY) && clit->triggering_location == LOCATION_HAND
 			        && (((peffect->type & EFFECT_TYPE_SINGLE) && !(peffect->flag & EFFECT_FLAG_SINGLE_RANGE) && peffect->handler->is_has_relation(peffect))
@@ -1909,11 +1912,12 @@ int32 field::process_point_event(int16 step, int32 skip_trigger, int32 skip_free
 		while(++clit != core.new_ochain_s.end()) {
 			++index;
 			peffect = clit->triggering_effect;
+			effect* preason_effect = peffect->handler->current.reason_effect;
 			if(pcard != peffect->handler)
 				continue;
 			bool act = true;
 			if(peffect->is_chainable(tp) && peffect->is_activateable(tp, clit->evt, TRUE)
-			        && ((peffect->code == EVENT_FLIP) || (clit->triggering_location & 0x3)
+			        && (((peffect->code == EVENT_FLIP) && preason_effect && (preason_effect->flag & EFFECT_FLAG_EHREN)) || (clit->triggering_location & 0x3)
 			            || !(peffect->handler->current.location & 0x3) || peffect->handler->is_status(STATUS_IS_PUBLIC))) {
 				if(!(peffect->flag & EFFECT_FLAG_FIELD_ONLY) && clit->triggering_location == LOCATION_HAND
 				        && (((peffect->type & EFFECT_TYPE_SINGLE) && !(peffect->flag & EFFECT_FLAG_SINGLE_RANGE) && peffect->handler->is_has_relation(peffect))
