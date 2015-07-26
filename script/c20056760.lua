46a8de3c8966b121056cf59db20d84d9f2bb47b6
salix5 <salix5@livemail.tw>
2015-07-26 16:22:11 +0800

    fix

--- script/c20056760.lua
+++ script/c20056760.lua
@@ -59,7 +59,7 @@ function c20056760.spcon2(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
 end
 function c20056760.spfilter(c,e,tp)
-	return c:IsSetCard(0xd0) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0xd1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c20056760.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c20056760.spfilter(chkc,e,tp) end
