2d64af0faee5e45ec0cf9142bf65bde67e3056d2
DailyShana <p2357111317192329@gmail.com>
2015-07-30 14:04:47 +0800

    fix

--- script/c6733059.lua
+++ script/c6733059.lua
@@ -6,7 +6,6 @@ function c6733059.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,0x1c0)
 	e1:SetCost(c6733059.cost1)
-	e1:SetOperation(c6733059.operation)
 	c:RegisterEffect(e1)
 	--instant(chain)
 	local e2=Effect.CreateEffect(c)
@@ -28,66 +27,48 @@ function c6733059.tfilter(c,lv)
 end
 function c6733059.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	e:SetLabel(0)
-	local cg=Duel.GetMatchingGroup(c6733059.cfilter,tp,LOCATION_GRAVE,0,nil)
-	local tg=Duel.GetMatchingGroup(c6733059.tfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,cg:GetCount())
-	if tg:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(6733059,0)) then
-		local lvt={}
-		local tc=tg:GetFirst()
-		while tc do
-			local tlv=tc:GetLevel()
-			lvt[tlv]=tlv
-			tc=tg:GetNext()
-		end
-		local pc=1
-		for i=1,12 do
-			if lvt[i] then lvt[i]=nil lvt[pc]=i pc=pc+1 end
-		end
-		lvt[pc]=nil
-		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(6733059,2))
-		local lv=Duel.AnnounceNumber(tp,table.unpack(lvt))
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-		local rg=cg:Select(tp,lv,lv,nil)
-		Duel.Remove(rg,POS_FACEUP,REASON_COST)
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,1,0,0)
-		Duel.SetTargetParam(lv)
+	if c6733059.cost2(e,tp,eg,ep,ev,re,r,rp,0) and Duel.SelectYesNo(tp,aux.Stringid(6733059,0)) then
+		e:SetCategory(CATEGORY_DESTROY)
+		e:SetOperation(c6733059.operation)
+		c6733059.cost2(e,tp,eg,ep,ev,re,r,rp,1)
+	else
+		e:SetCategory(0)
+		e:SetOperation(nil)
+		e:SetLabel(0)
 	end
 end
 function c6733059.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local cg=Duel.GetMatchingGroup(c6733059.cfilter,tp,LOCATION_GRAVE,0,nil)
 		local tg=Duel.GetMatchingGroup(c6733059.tfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,cg:GetCount())
-		return tg:GetCount()>0
+		return tg:GetCount()>0 and Duel.GetFlagEffect(tp,6733059)==0
 	end
-	e:SetLabel(0)
 	local cg=Duel.GetMatchingGroup(c6733059.cfilter,tp,LOCATION_GRAVE,0,nil)
 	local tg=Duel.GetMatchingGroup(c6733059.tfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,cg:GetCount())
 	local lvt={}
 	local tc=tg:GetFirst()
+	local i=1
 	while tc do
 		local tlv=tc:GetLevel()
-		lvt[tlv]=tlv
+		lvt[i]=tlv
 		tc=tg:GetNext()
+		i=i+1
 	end
-	local pc=1
-	for i=1,12 do
-		if lvt[i] then lvt[i]=nil lvt[pc]=i pc=pc+1 end
-	end
-	lvt[pc]=nil
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(6733059,2))
 	local lv=Duel.AnnounceNumber(tp,table.unpack(lvt))
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local rg=cg:Select(tp,lv,lv,nil)
 	Duel.Remove(rg,POS_FACEUP,REASON_COST)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,1,0,0)
-	Duel.SetTargetParam(lv)
+	e:SetLabel(lv)
+	Duel.RegisterFlagEffect(tp,6733059,RESET_CHAIN,0,1)
 end
 function c6733059.dfilter(c,lv)
 	return c:IsFaceup() and c:GetLevel()==lv and c:IsDestructable()
 end
 function c6733059.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local lv=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local lv=e:GetLabel()
 	if lv==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,c6733059.dfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,lv)
