c61b59263d0fa7cb8a5ac9842f741291aaadd589
Steeldarkeagel <steeldarkeagel@live.com>
2015-08-04 21:19:07 +0100

    Update c59718521.lua

--- script/c59718521.lua
+++ script/c59718521.lua
@@ -1,14 +1,14 @@
---ブローニング・パワー
-function c59718521.initial_effect(c)
+--ポリノシス
+function c91078716.initial_effect(c)
 	--Activate(summon)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DISABLE_SUMMON+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON)
-	e1:SetCondition(c59718521.condition1)
-	e1:SetCost(c59718521.cost)
-	e1:SetTarget(c59718521.target1)
-	e1:SetOperation(c59718521.activate1)
+	e1:SetCondition(c91078716.condition1)
+	e1:SetCost(c91078716.cost)
+	e1:SetTarget(c91078716.target1)
+	e1:SetOperation(c91078716.activate1)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON)
@@ -18,43 +18,43 @@ function c59718521.initial_effect(c)
 	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_ACTIVATE)
 	e3:SetCode(EVENT_CHAINING)
-	e3:SetCondition(c59718521.condition2)
-	e3:SetCost(c59718521.cost)
-	e3:SetTarget(c59718521.target2)
-	e3:SetOperation(c59718521.activate2)
+	e3:SetCondition(c91078716.condition2)
+	e3:SetCost(c91078716.cost)
+	e3:SetTarget(c91078716.target2)
+	e3:SetOperation(c91078716.activate2)
 	c:RegisterEffect(e3)
 end
-function c59718521.condition1(e,tp,eg,ep,ev,re,r,rp)
+function c91078716.condition1(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()==0
 end
-function c59718521.filter(c)
+function c91078716.filter(c)
 	return c:IsReleasable() and not c:IsStatus(STATUS_BATTLE_DESTROYED)
 end
-function c59718521.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(Card.IsRace,tp,LOCATION_MZONE,0,nil,RACE_PSYCHO)
-	if chk==0 then return g:GetCount()>0 and g:FilterCount(c59718521.filter,nil)==g:GetCount() end
+function c91078716.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c91078716.filter,1,nil,RACE_PLANT) end
+	local g=Duel.SelectReleaseGroup(tp,c91078716.filter,1,1,nil,RACE_PLANT)
 	Duel.Release(g,REASON_COST)
 end
-function c59718521.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+function c91078716.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,eg:GetCount(),0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,eg:GetCount(),0,0)
 end
-function c59718521.activate1(e,tp,eg,ep,ev,re,r,rp)
+function c91078716.activate1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateSummon(eg)
 	Duel.Destroy(eg,REASON_EFFECT)
 end
-function c59718521.condition2(e,tp,eg,ep,ev,re,r,rp)
+function c91078716.condition2(e,tp,eg,ep,ev,re,r,rp)
 	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
 end
-function c59718521.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+function c91078716.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function c59718521.activate2(e,tp,eg,ep,ev,re,r,rp)
+function c91078716.activate2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateActivation(ev)
 	if re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
