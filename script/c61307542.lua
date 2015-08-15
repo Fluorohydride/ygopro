ff7ddfdf093f306288e4a2d5b8052bb93e846aec
DailyShana <p2357111317192329@gmail.com>
2015-08-15 10:29:27 +0800

    fix

--- script/c61307542.lua
+++ script/c61307542.lua
@@ -42,11 +42,11 @@ end
 function c61307542.efilter(e,re)
 	return re:IsActiveType(TYPE_MONSTER) and re:GetOwner()~=e:GetOwner()
 end
-function c61307542.cfilter(c)
-	return c:IsType(TYPE_TRAP) and c:IsPreviousLocation(LOCATION_SZONE) and c:GetPreviousSequence()<5
+function c61307542.cfilter(c,tp)
+	return c:IsType(TYPE_TRAP) and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_SZONE) and c:GetPreviousSequence()<5
 end
 function c61307542.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c61307542.cfilter,1,nil)
+	return eg:IsExists(c61307542.cfilter,1,nil,tp)
 end
 function c61307542.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
