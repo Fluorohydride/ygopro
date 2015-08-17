2a49b3595b4eb933ef0a1c3202867a5632b30f90
nekrozar <unyuho.buster@gmail.com>
2015-08-17 10:34:19 +0900

    new
    PSYFrame SetCard: 0xc1

---
+++ script/c37192109.lua
@@ -0,0 +1,110 @@
+--PSYフレームロード・Ζ
+function c37192109.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
+	c:EnableReviveLimit()
+	--remove
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(37192109,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c37192109.rmtg)
+	e1:SetOperation(c37192109.rmop)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(37192109,1))
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetTarget(c37192109.thtg)
+	e2:SetOperation(c37192109.thop)
+	c:RegisterEffect(e2)
+end
+function c37192109.rmfilter(c)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsAbleToRemove()
+		and bit.band(c:GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_SPECIAL
+end
+function c37192109.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c37192109.rmfilter(chkc) end
+	if chk==0 then return e:GetHandler():IsAbleToRemove()
+		and Duel.IsExistingTarget(c37192109.rmfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,c37192109.rmfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	g:AddCard(e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,2,0,0)
+end
+function c37192109.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) then return end
+	local g=Group.FromCards(c,tc)
+	if Duel.Remove(g,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+		local fid=e:GetHandler():GetFieldID()
+		local og=Duel.GetOperatedGroup()
+		local oc=og:GetFirst()
+		while oc do
+			oc:RegisterFlagEffect(37192109,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY,0,2,fid)
+			oc=og:GetNext()
+		end
+		og:KeepAlive()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+		e1:SetReset(RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
+		e1:SetCountLimit(1)
+		e1:SetLabel(fid)
+		e1:SetLabelObject(og)
+		e1:SetCondition(c37192109.retcon)
+		e1:SetOperation(c37192109.retop)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function c37192109.retfilter(c,fid)
+	return c:GetFlagEffectLabel(37192109)==fid
+end
+function c37192109.retcon(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetTurnPlayer()~=tp then return false end
+	local g=e:GetLabelObject()
+	if not g:IsExists(c37192109.retfilter,1,nil,e:GetLabel()) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
+function c37192109.retop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local sg=g:Filter(c37192109.retfilter,nil,e:GetLabel())
+	g:DeleteGroup()
+	local tc=sg:GetFirst()
+	while tc do
+		Duel.ReturnToField(tc)
+		tc=sg:GetNext()
+	end
+end
+function c37192109.thfilter(c)
+	return c:IsSetCard(0xc1) and c:IsAbleToHand()
+end
+function c37192109.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c37192109.thfilter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return e:GetHandler():IsAbleToExtra()
+		and Duel.IsExistingTarget(c37192109.thfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
+	local g=Duel.SelectTarget(tp,c37192109.thfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+function c37192109.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SendtoDeck(c,nil,2,REASON_EFFECT)~=0
+		and c:IsLocation(LOCATION_EXTRA) and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
