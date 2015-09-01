7a741fe213167c65b6fc84dba5d3cafbb39123c6
nekrozar <unyuho.buster@gmail.com>
2015-09-01 17:49:42 +0900

    fix

--- script/c42776960.lua
+++ script/c42776960.lua
@@ -28,23 +28,27 @@ function c42776960.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_CHANGE_DAMAGE)
 		e1:SetRange(LOCATION_MZONE)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_AVAILABLE_BD+EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetReset(RESET_EVENT+0x1fe0000)
 		e1:SetTargetRange(1,0)
-		e1:SetValue(0)
-		tc:RegisterEffect(e1)
+		e1:SetValue(c42776960.val)
+		tc:RegisterEffect(e1,true)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
 		e2:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e2)
+		tc:RegisterEffect(e2,true)
 		local e3=Effect.CreateEffect(e:GetHandler())
 		e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 		e3:SetCode(EVENT_LEAVE_FIELD)
 		e3:SetLabel(1-tp)
 		e3:SetOperation(c42776960.leaveop)
 		e3:SetReset(RESET_EVENT+0xc020000)
-		tc:RegisterEffect(e3)
+		tc:RegisterEffect(e3,true)
 	end
+end
+function c42776960.val(e,re,val,r,rp,rc)
+	if e:GetHandlerPlayer()==e:GetOwnerPlayer() then return 0 end
+	return val
 end
 function c42776960.leaveop(e,tp,eg,ep,ev,re,r,rp)
 	local WIN_REASON_RELAY_SOUL=0x1a
