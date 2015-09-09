2d72c2efedc842e27192e4b56992987adcb6b901
salix5 <salix5@livemail.tw>
2015-09-09 22:25:40 +0800

    infos.shuffle_count
    1 in each main phase

--- ocgcore/playerop.cpp
+++ ocgcore/playerop.cpp
@@ -139,7 +139,7 @@ int32 field::select_idle_command(uint16 step, uint8 playerid) {
 			pduel->write_buffer8(1);
 		else
 			pduel->write_buffer8(0);
-		if(infos.shuffle_count < 1 && player[playerid].list_hand.size() > 1)
+		if(infos.shuffle_count < 2 && player[playerid].list_hand.size() > 1)
 			pduel->write_buffer8(1);
 		else
 			pduel->write_buffer8(0);
