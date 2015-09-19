a448120e7936b65626aa8172687e94f8c4a8370f
salix5 <salix5@livemail.tw>
2015-09-19 13:52:08 +0800

    fix

--- script/c7617062.lua
+++ script/c7617062.lua
@@ -9,7 +9,7 @@ function c7617062.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_ATTACK)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetTarget(c7617062.ftarget)
 	c:RegisterEffect(e2)
@@ -25,17 +25,25 @@ function c7617062.initial_effect(c)
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetCode(EFFECT_DIRECT_ATTACK)
-	e4:SetRange(LOCATION_SZONE)
+	e4:SetRange(LOCATION_FZONE)
 	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e4:SetTarget(c7617062.dirtg)
 	c:RegisterEffect(e4)
 	--pos
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e5:SetRange(LOCATION_SZONE)
+	e5:SetRange(LOCATION_FZONE)
 	e5:SetCode(EVENT_BATTLE_DAMAGE)
-	e5:SetOperation(c7617062.operation)
+	e5:SetOperation(c7617062.regop)
 	c:RegisterEffect(e5)
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e6:SetRange(LOCATION_FZONE)
+	e6:SetCode(EVENT_DAMAGE_STEP_END)
+	e6:SetCondition(c7617062.poscon)
+	e6:SetOperation(c7617062.posop)
+	e6:SetLabelObject(e5)
+	c:RegisterEffect(e6)
 end
 function c7617062.bttg(e,c)
 	return c:IsFacedown()
@@ -46,18 +54,20 @@ end
 function c7617062.ftarget(e,c)
 	return not c:IsSetCard(0x8d)
 end
-function c7617062.operation(e,tp,eg,ep,ev,re,r,rp)
+function c7617062.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc=eg:GetFirst()
-	if tc:IsRelateToBattle() and tc:IsFaceup() and tc:IsCanTurnSet() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_DAMAGE_STEP_END)
-		e1:SetOperation(c7617062.posop)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE)
-		tc:RegisterEffect(e1)
-	end
+	c:RegisterFlagEffect(7617062,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE,0,1)
+	eg:GetFirst():RegisterFlagEffect(7617063,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE,0,1)
+end
+function c7617062.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(7617062)>0
+end
+function c7617062.filter(c)
+	return c:GetFlagEffect(7617063)>0
 end
 function c7617062.posop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ChangePosition(e:GetHandler(),POS_FACEDOWN_DEFENCE)
+	local g=Duel.GetMatchingGroup(c7617062.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.ChangePosition(g,POS_FACEDOWN_DEFENCE)
+	end
 end
