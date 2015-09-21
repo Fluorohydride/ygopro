ead9e38187c7d610c7865b58e19aa5729f472b45
独孤朲 <cool2brother@gmail.com>
2015-09-21 22:03:43 +0800

    Merge pull request #1562 from nekrozar/patch-3
    new card VJMP

---
+++ script/c91718579.lua
@@ -0,0 +1,86 @@
+--ゴゴゴアリステラ＆デクシア
+function c91718579.initial_effect(c)
+	--untargetable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetCondition(c91718579.tgcon)
+	e1:SetValue(c91718579.atlimit)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SELECT_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e2:SetTargetRange(0,0xff)
+	e2:SetValue(c91718579.tglimit)
+	c:RegisterEffect(e2)
+	--effect gain
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(c91718579.effcon)
+	e3:SetOperation(c91718579.effop)
+	c:RegisterEffect(e3)
+end
+function c91718579.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x59)
+end
+function c91718579.tgcon(e)
+	return Duel.IsExistingMatchingCard(c91718579.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler())
+end
+function c91718579.atlimit(e,c)
+	return c:IsFaceup() and c:IsSetCard(0x59)
+end
+function c91718579.tglimit(e,re,c)
+	return c:IsFaceup() and c:IsSetCard(0x59) and c:IsType(TYPE_MONSTER)
+end
+function c91718579.effcon(e,tp,eg,ep,ev,re,r,rp)
+	local mg=e:GetHandler():GetReasonCard():GetMaterial()
+	return r==REASON_XYZ and mg:IsExists(Card.IsSetCard,mg:GetCount(),nil,0x59)
+end
+function c91718579.effop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,91718579)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(91718579,0))
+	e1:SetCategory(CATEGORY_POSITION+CATEGORY_DEFCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c91718579.poscon)
+	e1:SetTarget(c91718579.postg)
+	e1:SetOperation(c91718579.posop)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	rc:RegisterEffect(e1,true)
+	if not rc:IsType(TYPE_EFFECT) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CHANGE_TYPE)
+		e2:SetValue(TYPE_MONSTER+TYPE_EFFECT+TYPE_XYZ)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		rc:RegisterEffect(e2)
+	end
+end
+function c91718579.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_XYZ
+end
+function c91718579.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsPosition(POS_FACEUP_ATTACK) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsPosition,tp,0,LOCATION_MZONE,1,nil,POS_FACEUP_ATTACK) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectTarget(tp,Card.IsPosition,tp,0,LOCATION_MZONE,1,1,nil,POS_FACEUP_ATTACK)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+function c91718579.posop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc or tc:IsDefencePos() or not tc:IsRelateToEffect(e) then return end
+	if Duel.ChangePosition(tc,POS_FACEUP_DEFENCE)==0 then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_DEFENCE_FINAL)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	e1:SetValue(0)
+	tc:RegisterEffect(e1)
+end
