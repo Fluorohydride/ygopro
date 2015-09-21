c0035560a70fc0c01455999cc3eb008f6b8246fe
VanillaSalt <vanillasalt5197@gmail.com>
2015-09-21 15:47:40 +0900

    fix

--- script/c71408082.lua
+++ script/c71408082.lua
@@ -45,7 +45,7 @@ function c71408082.spfilter(c,e,tp)
 	return c:IsSetCard(0x3b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c71408082.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
 		and Duel.IsExistingMatchingCard(c71408082.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND)
 end
