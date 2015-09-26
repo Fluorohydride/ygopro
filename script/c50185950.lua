09a4e5a5aedb45e59227b6c3a2a4253c68847a95
mercury233 <me@mercury233.me>
2015-09-26 17:43:37 +0800

    fix conflict

--- script/c50185950.lua
+++ script/c50185950.lua
@@ -2,8 +2,10 @@
 function c50185950.initial_effect(c)
 	--draw
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(50185950,0))
 	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_RELEASE)
 	e1:SetTarget(c50185950.drtg)
 	e1:SetOperation(c50185950.drop)
@@ -34,7 +36,7 @@ function c50185950.filter(c,tp)
 end
 function c50185950.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return eg:IsExists(c50185950.filter,1,nil,tp) and e:GetHandler():IsAbleToRemove() end
-	return Duel.SelectYesNo(tp,aux.Stringid(50185950,0))
+	return Duel.SelectYesNo(tp,aux.Stringid(50185950,1))
 end
 function c50185950.repval(e,c)
 	return c50185950.filter(c,e:GetHandlerPlayer())
