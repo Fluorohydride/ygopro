30f9062ac3ba814b97d785260d91d86201d6537a
mercury233 <me@mercury233.me>
2015-12-16 16:21:24 +0800

    change tag room host when host leave

--- gframe/tag_duel.cpp
+++ gframe/tag_duel.cpp
@@ -151,9 +151,28 @@ void TagDuel::JoinGame(DuelPlayer* dp, void* pdata, bool is_creater) {
 }
 void TagDuel::LeaveGame(DuelPlayer* dp) {
 	if(dp == host_player) {
-		EndDuel();
-		NetServer::StopServer();
-	} else if(dp->type == NETPLAYER_TYPE_OBSERVER) {
+		int host_pos;
+		if(players[0] && dp->type != 0) {
+			host_pos = 0;
+			host_player = players[0];
+		} else if(players[1] && dp->type != 1) {
+			host_pos = 1;
+			host_player = players[1];
+		} else if(players[2] && dp->type != 2) {
+			host_pos = 2;
+			host_player = players[2];
+		} else if(players[3] && dp->type != 3) {
+			host_pos = 3;
+			host_player = players[3];
+		} else {
+			EndDuel();
+			NetServer::StopServer();
+		}
+		STOC_TypeChange sctc;
+		sctc.type = 0x10 | host_pos;
+		NetServer::SendPacketToPlayer(players[host_pos], STOC_TYPE_CHANGE, sctc);
+	}
+	if(dp->type == NETPLAYER_TYPE_OBSERVER) {
 		observers.erase(dp);
 		if(!pduel) {
 			STOC_HS_WatchChange scwc;
