b2d01a8fc36e0519346501c8206f1c9edc08fd99
edo9300 <edoardo762@gmail.com>
2016-12-17 18:24:22 +0100

    added the possibility for a negative original level

--- gframe/data_manager.cpp
+++ gframe/data_manager.cpp
@@ -32,7 +32,12 @@ bool DataManager::LoadDB(const char* file) {
 			cd.attack = sqlite3_column_int(pStmt, 5);
 			cd.defense = sqlite3_column_int(pStmt, 6);
 			unsigned int level = sqlite3_column_int(pStmt, 7);
-			cd.level = level & 0xff;
+			if((level & 0x80000000) != 0) {
+				level = -level;
+				cd.level = -(level & 0xff);
+			}
+			else
+				cd.level = level & 0xff;
 			cd.lscale = (level >> 24) & 0xff;
 			cd.rscale = (level >> 16) & 0xff;
 			cd.race = sqlite3_column_int(pStmt, 8);
