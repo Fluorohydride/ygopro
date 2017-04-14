3c94b4b50722e89b38abf1fc76e71e0479aa50b3
edo9300 <edoardo762@gmail.com>
2017-04-14 16:10:45 +0200

    Added replay mode messages

--- gframe/duelclient.cpp
+++ gframe/duelclient.cpp
@@ -3111,6 +3111,8 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 	}
 	case MSG_ROCK_PAPER_SCISSORS: {
 		/*int player = */mainGame->LocalPlayer(BufferIO::ReadInt8(pbuf));
+		if(mainGame->dInfo.isReplay && mainGame->dInfo.isReplaySkiping)
+			return true;
 		mainGame->gMutex.Lock();
 		mainGame->PopupElement(mainGame->wHand);
 		mainGame->gMutex.Unlock();
@@ -3118,6 +3120,8 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 	}
 	case MSG_HAND_RES: {
 		int res = BufferIO::ReadInt8(pbuf);
+		if(mainGame->dInfo.isReplay && mainGame->dInfo.isReplaySkiping)
+			return true;
 		mainGame->stHintMsg->setVisible(false);
 		mainGame->showcardcode = ((res & 0x3) - 1) + ((((res >> 2) & 0x3) - 1) << 16);
 		mainGame->showcarddif = 50;
