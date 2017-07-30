e271e2fb7282b4fb6deb8fb68508d5ca5545fc55
mercury233 <me@mercury233.me>
2017-07-30 13:35:18 +0800

    fix

--- gframe/duelclient.cpp
+++ gframe/duelclient.cpp
@@ -294,6 +294,7 @@ void DuelClient::HandleSTOCPacketLan(char* data, unsigned int len) {
 		mainGame->deckBuilder.result_string[0] = L'0';
 		mainGame->deckBuilder.result_string[1] = 0;
 		mainGame->deckBuilder.results.clear();
+		mainGame->deckBuilder.hovered_code = 0;
 		mainGame->deckBuilder.is_draging = false;
 		mainGame->deckBuilder.is_starting_dragging = false;
 		mainGame->deckBuilder.pre_mainc = deckManager.current_deck.main.size();
