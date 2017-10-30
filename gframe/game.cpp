3e07dd40b68e58c27f2267ea87b213fb396b05c7
edo9300 <edoardo762@gmail.com>
2017-10-30 22:49:16 +0100

    Added custom summoning music for specific monsters

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -705,10 +705,12 @@ void Game::MainLoop() {
 			driver->setMaterial(irr::video::IdentityMaterial);
 			driver->clearZBuffer();
 		} else if(is_building) {
+			engineSound->stopAllSounds();
 			DrawBackImage(imageManager.tBackGround_deck);
 			DrawDeckBd();
 			PlayMusic("./sound/deck.mp3", true);
 		} else {
+			engineSound->stopAllSounds();
 			DrawBackImage(imageManager.tBackGround_menu);
 			PlayMusic("./sound/menu.mp3", true);
 		}
@@ -765,6 +767,7 @@ void Game::MainLoop() {
 	usleep(500000);
 #endif
 	SaveConfig();
+	engineSound->drop();
 	engineMusic->drop();
 //	device->drop();
 }
@@ -1149,6 +1152,18 @@ void Game::SaveConfig() {
 	if (vol < 0) vol = 0; else if (vol > 100) vol = 100;
 	fprintf(fp, "volume = %d\n", vol);
 	fclose(fp);
+}
+bool Game::PlayChant(unsigned int code) {
+	char sound[1000];
+	sprintf(sound, "./sound/chants/%d.wav", code);
+	FILE *file = fopen(sound, "r");
+	if(file) {
+		fclose(file);
+		if (!engineSound->isCurrentlyPlaying(sound))
+			PlaySoundEffect(sound);
+		return true;
+	}
+	return false;
 }
 void Game::PlaySoundEffect(char* sound) {
 	if(chkEnableSound->isChecked()) {
