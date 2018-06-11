c37faae5cb4b1d1c7bade8ea25e96540b4e62872
edo9300 <edoardo762@gmail.com>
2018-06-11 17:22:42 +0200

    Missing updating shuffle set card in relay duel

--- gframe/relay_duel.cpp
+++ gframe/relay_duel.cpp
@@ -931,6 +931,7 @@ int RelayDuel::Analyze(char* msgbuffer, unsigned int len) {
 			break;
 		}
 		case MSG_SHUFFLE_SET_CARD: {
+			int loc = BufferIO::ReadInt8(pbuf);
 			count = BufferIO::ReadInt8(pbuf);
 			pbuf += count * 8;
 			NetServer::SendBufferToPlayer(players[startp[0]].player, STOC_GAME_MSG, offset, pbuf - offset);
@@ -939,8 +940,14 @@ int RelayDuel::Analyze(char* msgbuffer, unsigned int len) {
 					NetServer::ReSendToPlayer(players[i].player);
 			for(auto oit = observers.begin(); oit != observers.end(); ++oit)
 				NetServer::ReSendToPlayer(*oit);
-			RefreshMzone(0, 0x181fff, 0);
-			RefreshMzone(1, 0x181fff, 0);
+			if(loc == LOCATION_MZONE) {
+				RefreshMzone(0, 0x181fff, 0);
+				RefreshMzone(1, 0x181fff, 0);
+			}
+			else {
+				RefreshMzone(0, 0x181fff, 0);
+				RefreshMzone(1, 0x181fff, 0);
+			}
 			break;
 		}
 		case MSG_NEW_TURN: {
