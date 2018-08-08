19fbe73c0d1b976489f1ca7f26187d6fc5962fbc
Mercury233 <me@mercury233.me>
2018-08-08 13:08:19 +0800

    add new style of select option (#2126)

--- gframe/client_field.h
+++ gframe/client_field.h
@@ -97,6 +97,7 @@ public:
 	void ShowSelectCard(bool buttonok = false, bool chain = false);
 	void ShowChainCard();
 	void ShowLocationCard();
+	void ShowSelectOption(int select_hint = 0);
 	void ReplaySwap();
 	void RefreshAllCards();
 
@@ -138,6 +139,7 @@ public:
 	void ShowCancelOrFinishButton(int buttonOp);
 	void SetShowMark(ClientCard* pcard, bool enable);
 	void SetResponseSelectedCards() const;
+	void SetResponseSelectedOption() const;
 	void CancelOrFinish();
 };
 
