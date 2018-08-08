3b30d5df03f6e54322fce1253e450f3279c2ce6f
Mercury233 <me@mercury233.me>
2018-08-08 13:08:07 +0800

    typo

--- gframe/client_field.cpp
+++ gframe/client_field.cpp
@@ -592,7 +592,7 @@ void ClientField::ShowLocationCard() {
 }
 void ClientField::ShowSelectOption(int select_hint) {
 	selected_option = 0;
-	wchar_t textBuffer[356];
+	wchar_t textBuffer[256];
 	int count = select_options.size();
 	bool quickmode = (count <= 5);
 	mainGame->gMutex.Lock();
