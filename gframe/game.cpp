c4356969219fddb85cc974568e97f6080365b8c3
edo9300 <edoardo762@gmail.com>
2018-11-18 16:30:03 +0100

    Fixed function definition (#10)
    * Update game.cpp
    
    * Update game.h

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -1398,21 +1398,7 @@ void Game::FlashWindow() {
 	FlashWindowEx(&fi);
 #endif
 }
-bool Game::CheckRegEx(const wchar_t* text, const wchar_t* exp, bool exact) {
-	if(!gameConf.search_regex)
-		return false;
-	bool result;
-	try {
-		if(exact)
-			result = std::regex_match(text, std::wregex(exp));
-		else
-			result = std::regex_search(text, std::wregex(exp));
-	} catch(...) {
-		result = false;
-	}
-	return result;
-}
-bool Game::CheckRegEx(std::wstring text, const wchar_t* exp, bool exact) {
+bool Game::CheckRegEx(const std::wstring& text, const std::wstring& exp, bool exact) {
 	if(!gameConf.search_regex)
 		return false;
 	bool result;
--- gframe/game.h
+++ gframe/game.h
@@ -144,8 +144,7 @@ public:
 
 	int LocalPlayer(int player);
 	const wchar_t* LocalName(int local_player);
-	bool CheckRegEx(const wchar_t* text, const wchar_t* exp, bool exact = false);
-	bool CheckRegEx(std::wstring text, const wchar_t* exp, bool exact = false);
+	bool CheckRegEx(const std::wstring& text, const std::wstring& exp, bool exact);
 
 	bool HasFocus(EGUI_ELEMENT_TYPE type) const {
 		irr::gui::IGUIElement* focus = env->getFocus();
