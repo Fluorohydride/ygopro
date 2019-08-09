9000654388a866443fd518164c435dffb95ec0ff
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-08-09 10:53:13 -0400

    [Travis] Upgrade Linux build image to Bionic (#30)
    * [Travis] Stop trying to initialize script submodule, enable parallel Make
    
    * [Travis][Linux] Disable testing fmt build
    
    * [Travis][Windows] Standardize vcpkg install like ygopen
    
    cc DyXel/ygopen-gui#5
    
    * [Travis][Linux] Update from Xenial to Bionic

--- build-support/install-ubuntu-src.sh
+++ build-support/install-ubuntu-src.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 # Installs needed dependencies to /usr/local from source
 git clone --depth=1 --branch=master https://github.com/fmtlib/fmt.git /tmp/fmt
-cd /tmp/fmt
-cmake .
+mkdir -p /tmp/fmt/build
+cd /tmp/fmt/build
+cmake .. -DFMT_TEST=OFF
 make
 sudo make install
 
-curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
 sudo mkdir -p /usr/local/include/nlohmann
-sudo mv json.hpp /usr/local/include/nlohmann
+cd /usr/local/include/nlohmann
+sudo curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
--- build-support/install-windows-vcpkg-cache.sh
+++ build-support/install-windows-vcpkg-cache.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-git clone --depth=1 https://github.com/Microsoft/vcpkg.git $VCPKG_ROOT
-cd $VCPKG_ROOT
-curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://github.com/kevinlul/edopro-vcpkg-cache/raw/master/installed.zip
+set -euxo pipefail
+
+git clone --depth=1 https://github.com/Microsoft/vcpkg.git "$VCPKG_ROOT"
+cd "$VCPKG_ROOT"
+curl --retry 5 --connect-timeout 30 --location --remote-header-name --output installed.zip $VCPKG_CACHE_ZIP_URL
 unzip -uo installed.zip
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '.\bootstrap-vcpkg.bat'"
 ./vcpkg.exe integrate install
-cd -
-./build-support/install-windows-vcpkg.sh; 
+./vcpkg.exe install $VCPKG_LIBS
