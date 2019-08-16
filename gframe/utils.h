6ee5d267d7d7f6f9e37d2c06657c27d4264f564d
edo9300 <edoardo762@gmail.com>
2019-08-17 01:32:15 +0200

    Wrong function signature for non windows systems

--- gframe/utils.h
+++ gframe/utils.h
@@ -47,7 +47,7 @@ namespace ygo {
 		static void FindfolderFiles(const std::wstring & path, const std::function<void(std::wstring, bool, void*)>& cb, void* payload = nullptr);
 		static std::vector<std::wstring> FindfolderFiles(const std::wstring& path, std::vector<std::wstring> extensions, int subdirectorylayers = 0);
 		static void FindfolderFiles(IrrArchiveHelper& archive, const path_string& path, const std::function<bool(int, path_string, bool, void*)>& cb, void* payload = nullptr);
-		static std::vector<int> FindfolderFiles(IrrArchiveHelper& archive, const path_string& path, std::vector<std::wstring> extensions, int subdirectorylayers = 0);
+		static std::vector<int> FindfolderFiles(IrrArchiveHelper& archive, const path_string& path, std::vector<path_string> extensions, int subdirectorylayers = 0);
 		static irr::io::IReadFile* FindandOpenFileFromArchives(const path_string& path, const path_string& name);
 		static std::wstring NormalizePath(std::wstring path, bool trailing_slash = true);
 		static std::wstring GetFileExtension(std::wstring file);
