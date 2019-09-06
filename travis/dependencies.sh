5c6fa8695188e9ebab4583293355db73cf128af1
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-09-05 22:29:18 -0400

    Build against Lua with C++ bindings again due to longjmp UB

--- travis/dependencies.sh
+++ travis/dependencies.sh
@@ -3,14 +3,16 @@
 set -euxo pipefail
 
 ./travis/install-local-dependencies.sh
-if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then 
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
     ./travis/get-windows-vcpkg-cache.sh
     ./travis/get-windows-d3d9sdk.sh
 fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     ./travis/get-linux-vcpkg-cache.sh
-fi 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+#   ./ocgcore/travis/install-lua.sh
+fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     ./travis/get-osx-sdk.sh $MACOSX_DEPLOYMENT_TARGET
     ./travis/get-osx-cache.sh
+#   ./ocgcore/travis/install-lua.sh
 fi
