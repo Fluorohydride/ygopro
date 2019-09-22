620df07ae30f417772b313ab4ce829785cefcc96
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-09-22 12:52:57 -0400

    Fix errors with discord-launcher

--- discord_rpc_handle_macos/wrapper.m
+++ discord_rpc_handle_macos/wrapper.m
@@ -1,16 +1,17 @@
-#include <stdlib.h>
+#include <string.h>
 #import <AppKit/AppKit.h>
 
 int main(int argc, char* argv[]) {
-	if(argc == 1) {
-		char* name = [[[NSBundle mainBundle] bundleIdentifier] UTF8String];
-		int pos = strsrch(name, '.', -1);
-		if(pos != -1)
-			return 0;
-		name[pos] = 0;
-		char launch_argument[256];
-		sprintf("open -b %s --args from_discord", name);
-		system(launch_argument);
-	}
-	return 0;
+    (void) argv;
+    if(argc == 1) {
+        const char* name = [[[NSBundle mainBundle] bundleIdentifier] UTF8String];
+        char* last = strrchr(name, '.');
+        if(last == 0)
+            return 0;
+        *last = 0;
+        char launch_argument[256];
+        sprintf(launch_argument, "open -b %s --args from_discord", name);
+        system(launch_argument);
+    }
+    return 0;
 }
