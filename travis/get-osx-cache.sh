724138c8e9ed4b7f8f19bda2e72c5e1d7097e3cf
kevinlul <6320810+kevinlul@users.noreply.github.com>
2019-10-12 12:40:37 -0400

    [Travis][macOS] Automatically locate libevent cellar so we never have to hardcode this again

--- travis/get-osx-cache.sh
+++ travis/get-osx-cache.sh
@@ -8,4 +8,4 @@ curl --retry 5 --connect-timeout 30 --location --remote-header-name --output cac
 
 cp -r irrlicht/include /usr/local/include/irrlicht
 cp irrlicht/lib/* /usr/local/lib
-cp -r libevent/ /usr/local/Cellar/libevent/2.1.11_1
+sudo cp -r libevent/ $(brew --cellar libevent)/$(brew list --versions libevent | cut -f2 -d" ")
