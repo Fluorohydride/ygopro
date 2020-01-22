eb47e40103039c917abc45b7605c05748be1ef32
edo9300 <edoardo762@gmail.com>
2020-01-22 19:28:41 +0100

    Use inlined ocgcore function definitions

--- gframe/dllinterface.cpp
+++ gframe/dllinterface.cpp
@@ -1,142 +1,91 @@
 #ifdef YGOPRO_BUILD_DLL
-#include "dllinterface.h"
 #include <string>
-#include "utils.h"
-#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #else
 #ifdef __ANDROID__
-#include <fstream>
 #include "porting_android.h"
 #endif
 #include <dlfcn.h>
 #endif
+#include "dllinterface.h"
+#include "utils.h"
+#ifdef _WIN32
 
 #ifdef _WIN32
-#define CORENAME TEXT("ocgcore.dll")
+#define CORENAME EPRO_TEXT("ocgcore.dll")
 #elif defined(__APPLE__)
-#define CORENAME TEXT("libocgcore.dylib")
+#define CORENAME EPRO_TEXT("libocgcore.dylib")
 #elif defined(__ANDROID__)
 #if defined(__arm__)
-#define CORENAME TEXT("libocgcorev7.so")
+#define CORENAME EPRO_TEXT("libocgcorev7.so")
 #elif defined(__i386__)
-#define CORENAME TEXT("libocgcorex86.so")
+#define CORENAME EPRO_TEXT("libocgcorex86.so")
 #elif defined(__aarch64__)
-#define CORENAME TEXT("libocgcorev8.so")
+#define CORENAME EPRO_TEXT("libocgcorev8.so")
 #elif defined(__x86_64__)
-#define CORENAME TEXT("libocgcorex64.so")
+#define CORENAME EPRO_TEXT("libocgcorex64.so")
 #endif
 #else
-#define CORENAME TEXT("libocgcore.so")
+#define CORENAME EPRO_TEXT("libocgcore.so")
 #endif
 
-void(*OCG_GetVersion)(int* major, int* minor) = nullptr;
-
-int(*OCG_CreateDuel)(OCG_Duel* duel, OCG_DuelOptions options) = nullptr;
-void(*OCG_DestroyDuel)(OCG_Duel duel) = nullptr;
-void(*OCG_DuelNewCard)(OCG_Duel duel, OCG_NewCardInfo info) = nullptr;
-int(*OCG_StartDuel)(OCG_Duel duel) = nullptr;
-
-int(*OCG_DuelProcess)(OCG_Duel duel) = nullptr;
-void*(*OCG_DuelGetMessage)(OCG_Duel duel, uint32_t* length) = nullptr;
-void(*OCG_DuelSetResponse)(OCG_Duel duel, void* buffer, uint32_t length) = nullptr;
-int(*OCG_LoadScript)(OCG_Duel duel, const char* buffer, uint32_t length, const char* name) = nullptr;
-
-uint32_t(*OCG_DuelQueryCount)(OCG_Duel duel, uint8_t team, uint32_t loc) = nullptr;
-void*(*OCG_DuelQuery)(OCG_Duel duel, uint32_t* length, OCG_QueryInfo info) = nullptr;
-void*(*OCG_DuelQueryLocation)(OCG_Duel duel, uint32_t* length, OCG_QueryInfo info) = nullptr;
-void*(*OCG_DuelQueryField)(OCG_Duel duel, uint32_t* length) = nullptr;
+#define X(type,name,...) type(*name)(__VA_ARGS__) = nullptr;
+#include "ocgcore_functions.inl"
+#undef X
 
 #define CREATE_CLONE(x) auto x##_copy = x;
 #define STORE_CLONE(x) x##_copy = x;
 #define CLEAR_CLONE(x) x##_copy = nullptr;
 
-CREATE_CLONE(OCG_GetVersion)
-CREATE_CLONE(OCG_CreateDuel)
-CREATE_CLONE(OCG_DestroyDuel)
-CREATE_CLONE(OCG_DuelNewCard)
-CREATE_CLONE(OCG_StartDuel)
-CREATE_CLONE(OCG_DuelProcess)
-CREATE_CLONE(OCG_DuelGetMessage)
-CREATE_CLONE(OCG_DuelSetResponse)
-CREATE_CLONE(OCG_LoadScript)
-CREATE_CLONE(OCG_DuelQueryCount)
-CREATE_CLONE(OCG_DuelQuery)
-CREATE_CLONE(OCG_DuelQueryLocation)
-CREATE_CLONE(OCG_DuelQueryField)
+#define X(type,name,...) CREATE_CLONE(name)
+#include "ocgcore_functions.inl"
+#undef X
 #undef CREATE_CLONE
 
 #ifdef _WIN32
-void* OpenLibrary(const std::basic_string<TCHAR>& path) {
+void* OpenLibrary(const path_string& path) {
 	return LoadLibrary((path + CORENAME).c_str());
 }
-void CloseLibrary(void* handle) {
-	FreeLibrary((HMODULE)handle);
-}
+#define CloseLibrary(core) FreeLibrary((HMODULE)core)
 
-#define GetFunction(core, x) GetProcAddress((HMODULE)core, x)
+#define GetFunction(core, x) (decltype(x))GetProcAddress((HMODULE)core, #x)
 
 #else
 
-void* OpenLibrary(const std::string& path) {
+void* OpenLibrary(const path_string& path) {
 #ifdef __ANDROID__
 	void* lib = nullptr;
-	const auto dest_dir = porting::internal_storage + TEXT("/libocgcore.so");
-	std::ifstream  src(path + CORENAME, std::ios::binary);
-	std::ofstream  dst(dest_dir, std::ios::binary);
-	dst << src.rdbuf();
-	dst.close();
-	src.close();
+	const auto dest_dir = porting::internal_storage + EPRO_TEXT("/libocgcore.so");
+	Utils;; Copyfile(path + CORENAME, dest_dir);
 	lib = dlopen(dest_dir.c_str(), RTLD_LAZY);
-	remove(dest_dir.c_str());
+	Utils::Deletefile(dest_dir);
 	return lib;
 #else
 	return dlopen((path + CORENAME).c_str(), RTLD_LAZY);
 #endif
 }
 
-void CloseLibrary(void*handle) {
-	dlclose(handle);
-}
+#define CloseLibrary(core) dlclose(core)
 
-#define GetFunction(core, x) dlsym(core, x)
+#define GetFunction(core, x) (decltype(x))dlsym(core, #x)
 
 #endif
 
 #define RESTORE_CLONE(x) if(x##_copy) { x = x##_copy; x##_copy = nullptr; }
 
 void RestoreFromCopies() {
-	RESTORE_CLONE(OCG_GetVersion);
-	RESTORE_CLONE(OCG_CreateDuel);
-	RESTORE_CLONE(OCG_DestroyDuel);
-	RESTORE_CLONE(OCG_DuelNewCard);
-	RESTORE_CLONE(OCG_StartDuel);
-	RESTORE_CLONE(OCG_DuelProcess);
-	RESTORE_CLONE(OCG_DuelGetMessage);
-	RESTORE_CLONE(OCG_DuelSetResponse);
-	RESTORE_CLONE(OCG_LoadScript);
-	RESTORE_CLONE(OCG_DuelQueryCount);
-	RESTORE_CLONE(OCG_DuelQuery);
-	RESTORE_CLONE(OCG_DuelQueryLocation);
-	RESTORE_CLONE(OCG_DuelQueryField);
+#define X(type,name,...) RESTORE_CLONE(name)
+#include "ocgcore_functions.inl"
+#undef X
 }
 
 #undef RESTORE_CLONE
 
 void ClearCopies() {
-	CLEAR_CLONE(OCG_GetVersion);
-	CLEAR_CLONE(OCG_CreateDuel);
-	CLEAR_CLONE(OCG_DestroyDuel);
-	CLEAR_CLONE(OCG_DuelNewCard);
-	CLEAR_CLONE(OCG_StartDuel);
-	CLEAR_CLONE(OCG_DuelProcess);
-	CLEAR_CLONE(OCG_DuelGetMessage);
-	CLEAR_CLONE(OCG_DuelSetResponse);
-	CLEAR_CLONE(OCG_LoadScript);
-	CLEAR_CLONE(OCG_DuelQueryCount);
-	CLEAR_CLONE(OCG_DuelQuery);
-	CLEAR_CLONE(OCG_DuelQueryLocation);
-	CLEAR_CLONE(OCG_DuelQueryField);
+#define X(type,name,...) CLEAR_CLONE(name)
+#include "ocgcore_functions.inl"
+#undef X
 }
 
 bool check_api_version() {
@@ -146,63 +95,42 @@ bool check_api_version() {
 	return (max == OCG_VERSION_MAJOR) && (min == OCG_VERSION_MINOR);
 }
 
-#define LOAD_FUNCTION(x) x = (decltype(x))GetFunction(newcore, #x);\
+#define LOAD_FUNCTION(x) x = GetFunction(newcore, x);\
 		if(!x){ UnloadCore(newcore); return nullptr; }
 
-#ifdef UNICODE
-void* LoadOCGcore(const std::wstring& path) {
-#else
-void* LoadOCGcore(const std::string& path) {
-#endif
+void* LoadOCGcore(const path_string& path) {
 	void* newcore = OpenLibrary(path);
 	if(!newcore)
 		return nullptr;
-	LOAD_FUNCTION(OCG_GetVersion);
+	ClearCopies();
+#define X(type,name,...) LOAD_FUNCTION(name)
+#include "ocgcore_functions.inl"
+#undef X
 	if(!check_api_version()) {
 		UnloadCore(newcore);
 		return nullptr;
 	}
-	LOAD_FUNCTION(OCG_CreateDuel);
-	LOAD_FUNCTION(OCG_DestroyDuel);
-	LOAD_FUNCTION(OCG_DuelNewCard);
-	LOAD_FUNCTION(OCG_StartDuel);
-	LOAD_FUNCTION(OCG_DuelProcess);
-	LOAD_FUNCTION(OCG_DuelGetMessage);
-	LOAD_FUNCTION(OCG_DuelSetResponse);
-	LOAD_FUNCTION(OCG_LoadScript);
-	LOAD_FUNCTION(OCG_DuelQueryCount);
-	LOAD_FUNCTION(OCG_DuelQuery);
-	LOAD_FUNCTION(OCG_DuelQueryLocation);
-	LOAD_FUNCTION(OCG_DuelQueryField);
-	ClearCopies();
 	return newcore;
 }
 #undef LOAD_FUNCTION
 
 #define LOAD_WITH_COPY_CHECK(x) STORE_CLONE(x)\
-		x = (decltype(x))GetFunction(handle, #x);\
+		x = GetFunction(handle, x);\
 		if(!x) {\
-			UnloadCore(handle);\
 			RestoreFromCopies();\
 			return false;\
 		}
 
-bool ReloadCore(void*handle) {
+bool ReloadCore(void* handle) {
 	if(!handle)
 		return false;
-	LOAD_WITH_COPY_CHECK(OCG_GetVersion);
-	LOAD_WITH_COPY_CHECK(OCG_CreateDuel);
-	LOAD_WITH_COPY_CHECK(OCG_DestroyDuel);
-	LOAD_WITH_COPY_CHECK(OCG_DuelNewCard);
-	LOAD_WITH_COPY_CHECK(OCG_StartDuel);
-	LOAD_WITH_COPY_CHECK(OCG_DuelProcess);
-	LOAD_WITH_COPY_CHECK(OCG_DuelGetMessage);
-	LOAD_WITH_COPY_CHECK(OCG_DuelSetResponse);
-	LOAD_WITH_COPY_CHECK(OCG_LoadScript);
-	LOAD_WITH_COPY_CHECK(OCG_DuelQueryCount);
-	LOAD_WITH_COPY_CHECK(OCG_DuelQuery);
-	LOAD_WITH_COPY_CHECK(OCG_DuelQueryLocation);
-	LOAD_WITH_COPY_CHECK(OCG_DuelQueryField);
+#define X(type,name,...) LOAD_WITH_COPY_CHECK(name)
+#include "ocgcore_functions.inl"
+#undef X
+	if(!check_api_version()) {
+		RestoreFromCopies();
+		return false;
+	}
 	ClearCopies();
 	return true;
 }
@@ -211,59 +139,35 @@ bool ReloadCore(void*handle) {
 
 #define CLEAR_FUNCTION(x) x = nullptr;
 
-void UnloadCore(void*handle) {
+void UnloadCore(void* handle) {
 	CloseLibrary(handle);
-	CLEAR_FUNCTION(OCG_GetVersion);
-	CLEAR_FUNCTION(OCG_CreateDuel);
-	CLEAR_FUNCTION(OCG_DestroyDuel);
-	CLEAR_FUNCTION(OCG_DuelNewCard);
-	CLEAR_FUNCTION(OCG_StartDuel);
-	CLEAR_FUNCTION(OCG_DuelProcess);
-	CLEAR_FUNCTION(OCG_DuelGetMessage);
-	CLEAR_FUNCTION(OCG_DuelSetResponse);
-	CLEAR_FUNCTION(OCG_LoadScript);
-	CLEAR_FUNCTION(OCG_DuelQueryCount);
-	CLEAR_FUNCTION(OCG_DuelQuery);
-	CLEAR_FUNCTION(OCG_DuelQueryLocation);
-	CLEAR_FUNCTION(OCG_DuelQueryField);
+#define X(type,name,...) CLEAR_FUNCTION(name)
+#include "ocgcore_functions.inl"
+#undef X
 }
 
 #undef CLEAR_FUNCTION
 
 #define CHANGE_WITH_COPY_CHECK(x) STORE_CLONE(x)\
-		x = (decltype(x))GetFunction(newcore, #x);\
+		x = GetFunction(newcore, x);\
 		if(!x) {\
 			CloseLibrary(newcore);\
 			RestoreFromCopies();\
 			return nullptr;\
 		}
 
-#ifdef UNICODE
-void* ChangeOCGcore(const std::wstring& path, void*handle) {
-#else
-void* ChangeOCGcore(const std::string& path, void*handle) {
-#endif
+void* ChangeOCGcore(const path_string& path, void* handle) {
 	void* newcore = OpenLibrary(path);
 	if(!newcore)
 		return nullptr;
-	CHANGE_WITH_COPY_CHECK(OCG_GetVersion);
+#define X(type,name,...) CHANGE_WITH_COPY_CHECK(name)
+#include "ocgcore_functions.inl"
+#undef X
 	if(!check_api_version()) {
 		CloseLibrary(newcore);
 		RestoreFromCopies();
 		return nullptr;
 	}
-	CHANGE_WITH_COPY_CHECK(OCG_CreateDuel);
-	CHANGE_WITH_COPY_CHECK(OCG_DestroyDuel);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelNewCard);
-	CHANGE_WITH_COPY_CHECK(OCG_StartDuel);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelProcess);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelGetMessage);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelSetResponse);
-	CHANGE_WITH_COPY_CHECK(OCG_LoadScript);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelQueryCount);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelQuery);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelQueryLocation);
-	CHANGE_WITH_COPY_CHECK(OCG_DuelQueryField);
 	ClearCopies();
 	if(handle)
 		CloseLibrary(handle);
--- gframe/dllinterface.h
+++ gframe/dllinterface.h
@@ -4,33 +4,15 @@
 #include <ocgapi.h>
 #else
 #include "ocgapi_types.h"
-#include <string>
+#include "text_types.h"
 bool ReloadCore(void* handle);
 void UnloadCore(void *handle);
-#ifdef UNICODE
-void* LoadOCGcore(const std::wstring& path);
-void* ChangeOCGcore(const std::wstring& path, void* handle);
-#else
-void* LoadOCGcore(const std::string& path);
-void* ChangeOCGcore(const std::string& path, void* handle);
-#endif
+void* LoadOCGcore(const path_string& path);
+void* ChangeOCGcore(const path_string& path, void* handle);
 
-extern void(*OCG_GetVersion)(int* major, int* minor);
-
-extern int(*OCG_CreateDuel)(OCG_Duel* duel, OCG_DuelOptions options);
-extern void(*OCG_DestroyDuel)(OCG_Duel duel);
-extern void(*OCG_DuelNewCard)(OCG_Duel duel, OCG_NewCardInfo info);
-extern int(*OCG_StartDuel)(OCG_Duel duel);
-
-extern int(*OCG_DuelProcess)(OCG_Duel duel);
-extern void*(*OCG_DuelGetMessage)(OCG_Duel duel, uint32_t* length);
-extern void(*OCG_DuelSetResponse)(OCG_Duel duel, void* buffer, uint32_t length);
-extern int(*OCG_LoadScript)(OCG_Duel duel, const char* buffer, uint32_t length, const char* name);
-
-extern uint32_t(*OCG_DuelQueryCount)(OCG_Duel duel, uint8_t team, uint32_t loc);
-extern void*(*OCG_DuelQuery)(OCG_Duel duel, uint32_t* length, OCG_QueryInfo info);
-extern void*(*OCG_DuelQueryLocation)(OCG_Duel duel, uint32_t* length, OCG_QueryInfo info);
-extern void*(*OCG_DuelQueryField)(OCG_Duel duel, uint32_t* length);
+#define X(type,name,...) extern type(*name)(__VA_ARGS__);
+#include "ocgcore_functions.inl"
+#undef X
 
 #endif //YGOPRO_BUILD_DLL
 #endif /* DLL_INTERFACE_H_ */
