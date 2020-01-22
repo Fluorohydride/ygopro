d9d569ce5dac2894337d67ad98b1c2b88ceca380
edo9300 <edoardo762@gmail.com>
2020-01-22 23:48:47 +0100

    Fixed shadow of side count

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -1237,7 +1237,7 @@ void Game::DrawDeckBd() {
 	} else {
 		buffer = fmt::to_wstring(deckManager.current_deck.side.size());
 	}
-	numFont->draw(buffer.c_str(), Resize(379, 537, 439, 557), 0xff000000, false, true);
+	numFont->draw(buffer.c_str(), Resize(380, 538, 439, 557), 0xff000000, false, true);
 	numFont->draw(buffer.c_str(), Resize(379, 537, 439, 557), 0xffffffff, false, true);
 	recti sidepos = Resize(310, 537, 797, 557);
 	buffer = fmt::format(L"{} {} {} {} {} {}", dataManager.GetSysString(1312), deckManager.TypeCount(deckManager.current_deck.side, TYPE_MONSTER),
