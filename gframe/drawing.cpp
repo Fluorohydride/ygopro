bb004cfc97ed7ac8a404e4de143c698710a992aa
edo9300 <edoardo762@gmail.com>
2020-02-06 02:16:52 +0100

    Align deck search results count text properly

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -1286,8 +1286,9 @@ void Game::DrawDeckBd() {
 	driver->draw2DRectangleOutline(Resize(804, 136, 915, 157));
 	textFont->draw(dataManager.GetSysString(1333).c_str(), Resize(809, 136, 914, 156), 0xff000000, false, true);
 	textFont->draw(dataManager.GetSysString(1333).c_str(), Resize(810, 137, 915, 157), 0xffffffff, false, true);
-	numFont->draw(deckBuilder.result_string.c_str(), Resize(874, 136, 934, 156), 0xff000000, false, true);
-	numFont->draw(deckBuilder.result_string.c_str(), Resize(875, 137, 935, 157), 0xffffffff, false, true);
+	auto size = textFont->getDimension(dataManager.GetSysString(1333).c_str()).Width + ResizeX(5);
+	numFont->draw(deckBuilder.result_string.c_str(), Resize(809 + size, 136, 934, 156), 0xff000000, false, true);
+	numFont->draw(deckBuilder.result_string.c_str(), Resize(810 + size, 137, 935, 157), 0xffffffff, false, true);
 	DRAWRECT(SEARCH_RESULT, 805, 160, 1020, 630);
 	driver->draw2DRectangleOutline(Resize(804, 159, 1020, 630));
 	int prev_pos = deckBuilder.scroll_pos;
