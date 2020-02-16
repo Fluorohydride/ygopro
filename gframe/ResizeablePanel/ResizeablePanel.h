471d251479b5da934167d5b69aff1e139697c5ba
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-02-15 20:11:59 -0500

    Fixed non-const parameter in factory function for ResizeablePanel

--- gframe/ResizeablePanel/ResizeablePanel.h
+++ gframe/ResizeablePanel/ResizeablePanel.h
@@ -11,7 +11,7 @@ namespace gui {
 	class Panel : public IGUIElement {
 	public:
 		Panel(IGUIEnvironment* env, const core::rect<s32>& rectangle, s32 id = -1, IGUIElement * parent = nullptr, bool vertical = true, bool horizontal = true);
-		static Panel* addPanel(IGUIEnvironment* environment, IGUIElement* parent, s32 id, core::rect<s32>& rectangle, bool vertical = true, bool horizontal = true);
+		static Panel* addPanel(IGUIEnvironment* environment, IGUIElement* parent, s32 id, const core::rect<s32>& rectangle, bool vertical = true, bool horizontal = true);
 		IGUIElement* getSubpanel();
 		//virtual void draw();
 		virtual bool OnEvent(const SEvent& event);
