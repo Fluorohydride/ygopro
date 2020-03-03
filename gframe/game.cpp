48282294e00b88a32df7c9e178e8c4161770f626
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-03-03 12:53:12 -0500

    Patch version bump 37.0.2 -> 37.0.3

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -52,7 +52,7 @@
 unsigned short PRO_VERSION = 0x1350;
 #define EDOPRO_VERSION_MAJOR 37
 #define EDOPRO_VERSION_MINOR 0
-#define EDOPRO_VERSION_PATCH 2
+#define EDOPRO_VERSION_PATCH 3
 #define EDOPRO_VERSION_CODENAME L"Spider Shark"
 
 nlohmann::json configs;
