efbcb0d3013a439573b46fa65d15c54c5462bd6f
LordYuuma <lordyuuma@gmail.com>
2020-03-17 13:43:02 +0100

    premake: Add option for environment paths
    * premake4.lua [linux](environment-paths): New option.

--- premake4.lua
+++ premake4.lua
@@ -20,6 +20,14 @@ solution "ygo"
 
     configuration "linux"
         defines { "LUA_USE_LINUX" }
+        newoption
+        {
+            trigger = "environment-paths",
+            description = "Read databases, scripts and images from YGOPRO_*_PATH"
+        }
+        if _OPTIONS["environment-paths"] then
+            defines { "YGOPRO_ENVIRONMENT_PATHS" }
+        end
 
     configuration "vs*"
         flags "EnableSSE2"
