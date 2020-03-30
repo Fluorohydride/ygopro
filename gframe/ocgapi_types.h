6323b535a178f0a549d956062a7e6c72b77f8d8e
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-03-30 01:23:15 -0400

    Bump ocgcore for MR2020 rulings

--- gframe/ocgapi_types.h
+++ gframe/ocgapi_types.h
@@ -50,7 +50,7 @@ typedef struct OCG_Player {
 }OCG_Player;
 
 typedef void (*OCG_DataReader)(void* payload, int code, OCG_CardData* data);
-typedef void (*OCG_DataReaderDone)(void* payload);
+typedef void (*OCG_DataReaderDone)(void* payload, OCG_CardData* data);
 typedef int (*OCG_ScriptReader)(void* payload, OCG_Duel duel, const char* name);
 typedef void (*OCG_LogHandler)(void* payload, const char* string, int type);
 
