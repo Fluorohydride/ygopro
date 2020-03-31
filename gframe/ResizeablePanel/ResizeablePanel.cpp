6c19abc3e3f2bc8ebcf2e8e9907a5bafb4bdf07d
edo9300 <edoardo762@gmail.com>
2020-03-31 10:29:22 +0200

    Minor improvements to Panel

--- gframe/ResizeablePanel/ResizeablePanel.cpp
+++ gframe/ResizeablePanel/ResizeablePanel.cpp
@@ -17,7 +17,7 @@ namespace gui {
 		verticalScroll->setVisible(false);
 		horizontalScroll = Environment->addScrollBar(true, { 0, 0, 1, 1 }, this, -1);
 		horizontalScroll->setVisible(false);
-		subpanel = new SubPanel(env, parent, id, rectangle);
+		subpanel = new SubPanel(env, this, id, { 0,0,AbsoluteRect.getWidth(),AbsoluteRect.getHeight() });
 		subpanel->drop();
 	}
 	Panel* Panel::addPanel(IGUIEnvironment* environment, IGUIElement* parent, s32 id, const core::rect<s32>& rectangle, bool vertical, bool horizontal) {
@@ -45,7 +45,8 @@ namespace gui {
 							auto rect = getSubPaneRect();
 							auto horizontal_size = horizontalScroll->isVisible() ? horizontalScroll->getPos() : 0;
 							auto vertical_size = verticalScroll->isVisible() ? verticalScroll->getPos() : 0;
-							subpanel->setRelativePosition({ -horizontal_size, -vertical_size, rect.getWidth() + horizontal_size, rect.getHeight() + vertical_size });
+							rect.UpperLeftCorner -= irr::core::vector2di({horizontal_size, vertical_size});
+							subpanel->setRelativePosition(rect);
 						}
 					}
 					default:
@@ -62,24 +63,22 @@ namespace gui {
 		doResizingStuff();
 	}
 	irr::core::recti Panel::getSubPaneRect() {
-		auto rect = RelativeRect;
+		core::rect<s32> rect = { 0,0,AbsoluteRect.getWidth(),AbsoluteRect.getHeight() };
 		if(horizontalScroll->isVisible()) {
 			rect.LowerRightCorner.Y -= Environment->getSkin()->getSize(EGDS_SCROLLBAR_SIZE);
-			verticalScroll->setMax(verticalScroll->getMax() + 20);
 		}
 		if(verticalScroll->isVisible()) {
 			rect.LowerRightCorner.X -= (Environment->getSkin()->getSize(EGDS_SCROLLBAR_SIZE) + 10);
-			horizontalScroll->setMax(horizontalScroll->getMax() + 30);
 		}
 		return rect;
 	}
 	void Panel::doResizingStuff() {
 		auto scrollsize = Environment->getSkin()->getSize(EGDS_SCROLLBAR_SIZE);
-		subpanel->setRelativePosition(RelativeRect);
 		if(hasVerticalScrolling || hasHorizontalScrolling) {
 			horizontalScroll->setVisible(false);
 			verticalScroll->setVisible(false);
-			auto& rect = AbsoluteClippingRect;
+			const auto& rect = AbsoluteClippingRect;
+			const auto& absRect = AbsoluteRect;
 			s32 maxX = rect.LowerRightCorner.X;
 			s32 maxY = rect.LowerRightCorner.Y;
 			for(auto& child : subpanel->getChildren()) {
@@ -87,19 +86,20 @@ namespace gui {
 				maxY = std::max(maxY, child->getAbsolutePosition().LowerRightCorner.Y);
 			}
 			if(hasHorizontalScrolling && maxX > rect.LowerRightCorner.X) {
-				horizontalScroll->setRelativePosition(irr::core::rect<s32>(5, rect.getHeight() - (5 + scrollsize), rect.getWidth() - 5, rect.getHeight() - 5));
+				horizontalScroll->setRelativePosition(irr::core::rect<s32>(5, absRect.getHeight() - (5 + scrollsize), rect.getWidth() - 5, absRect.getHeight() - 5));
 				horizontalScroll->setVisible(true);
 				horizontalScroll->setMax(maxX - rect.LowerRightCorner.X);
 				horizontalScroll->setPos(0);
 			}
 			if(hasVerticalScrolling && maxY > rect.LowerRightCorner.Y) {
-				verticalScroll->setRelativePosition(irr::core::rect<s32>(rect.getWidth() - (5 + scrollsize), 10, rect.getWidth() - 5, rect.getHeight() - 25));
+				verticalScroll->setRelativePosition(irr::core::rect<s32>(absRect.getWidth() - (5 + scrollsize), 10, absRect.getWidth() - 5, rect.getHeight() - 25));
 				verticalScroll->setVisible(true);
 				verticalScroll->setMax(maxY - rect.LowerRightCorner.Y);
 				verticalScroll->setPos(0);
 			}
 			subpanel->setRelativePosition(getSubPaneRect());
-		}
+		} else
+			subpanel->setRelativePosition({ 0,0,AbsoluteRect.getWidth(),AbsoluteRect.getHeight() });
 	}
 } // end namespace gui
 } // end namespace irr
