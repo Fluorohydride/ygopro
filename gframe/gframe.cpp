98d559931b180005b0ef2f5414cba2181e120874
edo9300 <edoardo762@gmail.com>
2020-04-10 16:51:32 +0200

    Update gframe.cpp

--- gframe/gframe.cpp
+++ gframe/gframe.cpp
@@ -225,7 +225,6 @@ int main(int argc, char* argv[]) {
 	setlocale(LC_CTYPE, "UTF-8");
 	evthread_use_pthreads();
 #endif //_WIN32
-	ygo::updater::CheckUpdates();
 	std::shared_ptr<ygo::DataHandler> data = nullptr;
 	try {
 		data = std::make_shared<ygo::DataHandler>();
