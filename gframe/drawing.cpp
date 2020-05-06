032b1f7346b76672da8eccfaffb35ecdd2d09f1d
Mercury233 <me@mercury233.me>
2020-05-06 11:50:34 +0800

    always show chain mark (#2294)

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -457,28 +457,26 @@ void Game::DrawMisc() {
 		driver->setTransform(irr::video::ETS_WORLD, im);
 		driver->drawVertexPrimitiveList(matManager.vActivate, 4, matManager.iRectangle, 2);
 	}
-	if(dField.chains.size() > 1) {
-		for(size_t i = 0; i < dField.chains.size(); ++i) {
-			if(dField.chains[i].solved)
-				break;
-			matManager.mTRTexture.setTexture(0, imageManager.tChain);
-			matManager.mTRTexture.AmbientColor = 0xffffff00;
-			ic.setRotationRadians(act_rot);
-			ic.setTranslation(dField.chains[i].chain_pos);
-			driver->setMaterial(matManager.mTRTexture);
-			driver->setTransform(irr::video::ETS_WORLD, ic);
-			driver->drawVertexPrimitiveList(matManager.vSymbol, 4, matManager.iRectangle, 2);
-			it.setScale(0.6f);
-			it.setTranslation(dField.chains[i].chain_pos);
-			matManager.mTRTexture.setTexture(0, imageManager.tNumber);
-			matManager.vChainNum[0].TCoords = vector2df(0.19375f * (i % 5), 0.2421875f * (i / 5));
-			matManager.vChainNum[1].TCoords = vector2df(0.19375f * (i % 5 + 1), 0.2421875f * (i / 5));
-			matManager.vChainNum[2].TCoords = vector2df(0.19375f * (i % 5), 0.2421875f * (i / 5 + 1));
-			matManager.vChainNum[3].TCoords = vector2df(0.19375f * (i % 5 + 1), 0.2421875f * (i / 5 + 1));
-			driver->setMaterial(matManager.mTRTexture);
-			driver->setTransform(irr::video::ETS_WORLD, it);
-			driver->drawVertexPrimitiveList(matManager.vChainNum, 4, matManager.iRectangle, 2);
-		}
+	for(size_t i = 0; i < dField.chains.size(); ++i) {
+		if(dField.chains[i].solved)
+			break;
+		matManager.mTRTexture.setTexture(0, imageManager.tChain);
+		matManager.mTRTexture.AmbientColor = 0xffffff00;
+		ic.setRotationRadians(act_rot);
+		ic.setTranslation(dField.chains[i].chain_pos);
+		driver->setMaterial(matManager.mTRTexture);
+		driver->setTransform(irr::video::ETS_WORLD, ic);
+		driver->drawVertexPrimitiveList(matManager.vSymbol, 4, matManager.iRectangle, 2);
+		it.setScale(0.6f);
+		it.setTranslation(dField.chains[i].chain_pos);
+		matManager.mTRTexture.setTexture(0, imageManager.tNumber);
+		matManager.vChainNum[0].TCoords = vector2df(0.19375f * (i % 5), 0.2421875f * (i / 5));
+		matManager.vChainNum[1].TCoords = vector2df(0.19375f * (i % 5 + 1), 0.2421875f * (i / 5));
+		matManager.vChainNum[2].TCoords = vector2df(0.19375f * (i % 5), 0.2421875f * (i / 5 + 1));
+		matManager.vChainNum[3].TCoords = vector2df(0.19375f * (i % 5 + 1), 0.2421875f * (i / 5 + 1));
+		driver->setMaterial(matManager.mTRTexture);
+		driver->setTransform(irr::video::ETS_WORLD, it);
+		driver->drawVertexPrimitiveList(matManager.vChainNum, 4, matManager.iRectangle, 2);
 	}
 	//finish button
 	if(btnCancelOrFinish->isVisible() && dField.select_ready)
