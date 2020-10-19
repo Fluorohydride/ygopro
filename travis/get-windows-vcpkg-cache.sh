b563805c2ce33a310ca017fa60ce92aeaa04567b
edo9300 <edoardo762@gmail.com>
2020-10-19 16:12:36 +0200

    Create dummy ports and triplets folders to allow vcpkg to be installed

--- travis/get-windows-vcpkg-cache.sh
+++ travis/get-windows-vcpkg-cache.sh
@@ -6,4 +6,7 @@ mkdir -p "$VCPKG_ROOT"
 cd "$VCPKG_ROOT"
 curl --retry 5 --connect-timeout 30 --location --remote-header-name --output installed.zip $VCPKG_CACHE_ZIP_URL
 unzip -uo installed.zip > /dev/null
+mkdir ports
+mkdir triplets
+mkdir triplets/community
 ./vcpkg.exe integrate install
