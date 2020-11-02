6d659912026c72ce445d8d0926e88944f028d834
edo9300 <edoardo762@gmail.com>
2020-11-02 14:57:52 +0100

    Update vcpkg cache release

--- travis/get-osx-vcpkg-cache.sh
+++ travis/get-osx-vcpkg-cache.sh
@@ -8,7 +8,3 @@ curl --retry 5 --connect-timeout 30 --location --remote-header-name --output ins
 7z x installed.7z
 mv irrlicht/include /usr/local/include/irrlicht
 mv irrlicht/lib/* /usr/local/lib
-rm -rf installed/x64-osx/include/freetype
-rm installed/x64-osx/include/ft2build.h
-rm installed/x64-osx/lib/libfreetype.a
-rm installed/x64-osx/debug/lib/libfreetyped.a
