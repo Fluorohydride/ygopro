248c3303319c68dea94fd0ba4950650f0d85bc9c
edo9300 <edoardo762@gmail.com>
2020-12-22 23:36:14 +0100

    Update config.h

--- gframe/config.h
+++ gframe/config.h
@@ -11,9 +11,9 @@ extern bool open_file;
 extern epro::path_string open_file_name;
 extern bool show_changelog;
 
-#define EDOPRO_VERSION_MAJOR 38
-#define EDOPRO_VERSION_MINOR 1
-#define EDOPRO_VERSION_PATCH 3
+#define EDOPRO_VERSION_MAJOR 39
+#define EDOPRO_VERSION_MINOR 0
+#define EDOPRO_VERSION_PATCH 0
 #define EDOPRO_VERSION_CODENAME L"Hope Harbinger"
 #define CLIENT_VERSION (EDOPRO_VERSION_MAJOR & 0xff | ((EDOPRO_VERSION_MINOR & 0xff) << 8) | ((OCG_VERSION_MAJOR & 0xff) << 16) | ((OCG_VERSION_MINOR & 0xff) << 24))
 #define EXPAND_VERSION(ver) (ver) & 0xff, (((ver) >> 8) & 0xff), (((ver) >> 16) & 0xff), (((ver) >> 24) & 0xff)
