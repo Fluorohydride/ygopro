e158311f20b3e5b4455c7d1131aa5ce77f6ebce3
edo9300 <edoardo762@gmail.com>
2020-12-29 14:53:22 +0100

    Bump patch version

--- gframe/config.h
+++ gframe/config.h
@@ -13,7 +13,7 @@ extern bool show_changelog;
 
 #define EDOPRO_VERSION_MAJOR 39
 #define EDOPRO_VERSION_MINOR 0
-#define EDOPRO_VERSION_PATCH 1
+#define EDOPRO_VERSION_PATCH 2
 #define EDOPRO_VERSION_CODENAME L"Utopia"
 #define CLIENT_VERSION (EDOPRO_VERSION_MAJOR & 0xff | ((EDOPRO_VERSION_MINOR & 0xff) << 8) | ((OCG_VERSION_MAJOR & 0xff) << 16) | ((OCG_VERSION_MINOR & 0xff) << 24))
 #define EXPAND_VERSION(ver) (ver) & 0xff, (((ver) >> 8) & 0xff), (((ver) >> 16) & 0xff), (((ver) >> 24) & 0xff)
