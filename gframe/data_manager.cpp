b60b50f5698c571a6d302c8901f954d3dc6ea8ca
Edoardo Lolletti <edoardo762@gmail.com>
2021-01-26 23:22:13 +0100

    Update data_manager.cpp

--- gframe/data_manager.cpp
+++ gframe/data_manager.cpp
@@ -11,6 +11,8 @@
 
 namespace ygo {
 
+constexpr wchar_t DataManager::unknown_string[];
+
 static constexpr const char SELECT_STMT[] =
 R"(
 SELECT datas.id,datas.ot,datas.alias,datas.setcode,datas.type,datas.atk,datas.def,datas.level,datas.race,datas.attribute,datas.category,texts.name,texts.desc,texts.str1,texts.str2,texts.str3,texts.str4,texts.str5,texts.str6,texts.str7,texts.str8,texts.str9,texts.str10,texts.str11,texts.str12,texts.str13,texts.str14,texts.str15,texts.str16
