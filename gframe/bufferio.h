dbe3cbceea534f82fd2d5e4eb4815c24de298d8e
Edoardo Lolletti <edoardo762@gmail.com>
2021-01-26 21:39:11 +0100

    Properly preallocate string size for utf8-16/32 conversion
    uft8 to 16-32 should at max be the exact size, uft16 to utf8 should at max be twice its size, utf32 to utf8 at least 4 times its size

--- gframe/bufferio.h
+++ gframe/bufferio.h
@@ -127,18 +127,18 @@ public:
 	}
 	static std::string EncodeUTF8s(epro::wstringview source) {
 		thread_local std::vector<char> res;
-		res.reserve(source.size() * 4 + 1);
+#if	WCHAR_MAX == 0xffff
+			res.reserve(source.size() * 2 + 1);
+#else
+			res.reserve(source.size() * 4 + 1);
+#endif
 		EncodeUTF8(source.data(), const_cast<char*>(res.data()));
 		return res.data();
 	}
 	// UTF-8 to UTF-16/UTF-32
 	static std::wstring DecodeUTF8s(epro::stringview source) {
 		thread_local std::vector<wchar_t> res;
-#if	WCHAR_MAX == 0xffff
-			res.reserve(source.size() * 2 + 1);
-#else
-			res.reserve(source.size() + 1);
-#endif
+		res.reserve(source.size() + 1);
 		DecodeUTF8(source.data(), const_cast<wchar_t*>(res.data()));
 		return res.data();
 	}
