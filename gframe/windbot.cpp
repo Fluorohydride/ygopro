51c2f83c43ff836fdd54a363d708245ce5052a75
Edoardo Lolletti <edoardo762@gmail.com>
2021-02-10 17:09:28 +0100

    Check for forket windbot process to have correctly spawned on posix
    Call waitpid to check if the process exited abruptely (failed exec) and in case set its pid to 0

--- gframe/windbot.cpp
+++ gframe/windbot.cpp
@@ -3,11 +3,11 @@
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-#ifdef __ANDROID__
+#elif defined(__ANDROID__)
 #include "Android/porting_android.h"
+#else
+#include <sys/wait.h>
+#include <unistd.h>
 #endif
 #include <fmt/format.h>
 #include "config.h"
@@ -21,25 +21,22 @@ uint32_t WindBot::version{ CLIENT_VERSION };
 
 #if defined(_WIN32) || defined(__ANDROID__)
 bool WindBot::Launch(int port, const std::wstring& pass, bool chat, int hand) const {
-#else
-pid_t WindBot::Launch(int port, const std::wstring& pass, bool chat, int hand) const {
-#endif
 #ifdef _WIN32
-	auto args = fmt::format(
-		L"./WindBot/WindBot.exe HostInfo=\"{}\" Deck=\"{}\" Port={} Version={} name=\"[AI] {}\" Chat={} {}",
+	auto args = Utils::ToPathString(fmt::format(
+		L"./WindBot/WindBot.exe HostInfo=\"{}\" Deck=\"{}\" Port={} Version={} name=\"[AI] {}\" Chat={}{}",
 		pass,
 		deck,
 		port,
 		version,
 		name,
 		chat,
-		hand ? fmt::format(L"Hand={}", hand) : L"");
-	STARTUPINFO si = {};
-	PROCESS_INFORMATION pi = {};
+		hand ? fmt::format(L" Hand={}", hand) : L""));
+	STARTUPINFO si{};
+	PROCESS_INFORMATION pi{};
 	si.cb = sizeof(si);
 	si.dwFlags = STARTF_USESHOWWINDOW;
 	si.wShowWindow = SW_HIDE;
-	if (CreateProcess(NULL, (TCHAR*)Utils::ToPathString(args).data(), NULL, NULL, FALSE, 0, NULL, executablePath.data(), &si, &pi)) {
+	if(CreateProcess(nullptr, (TCHAR*)args.data(), nullptr, nullptr, FALSE, 0, nullptr, executablePath.data(), &si, &pi)) {
 		CloseHandle(pi.hProcess);
 		CloseHandle(pi.hThread);
 		return true;
@@ -57,7 +54,10 @@ pid_t WindBot::Launch(int port, const std::wstring& pass, bool chat, int hand) c
 		hand);
 	porting::launchWindbot(param);
 	return true;
+#endif
+}
 #else
+pid_t WindBot::Launch(int port, const std::wstring& pass, bool chat, int hand) const {
 	std::string argPass = fmt::format("HostInfo={}", BufferIO::EncodeUTF8s(pass));
 	std::string argDeck = fmt::format("Deck={}", BufferIO::EncodeUTF8s(deck));
 	std::string argPort = fmt::format("Port={}", port);
@@ -65,21 +65,25 @@ pid_t WindBot::Launch(int port, const std::wstring& pass, bool chat, int hand) c
 	std::string argName = fmt::format("name=[AI] {}", BufferIO::EncodeUTF8s(name));
 	std::string argChat = fmt::format("Chat={}", chat);
 	std::string argHand = fmt::format("Hand={}", hand);
-	std::string oldpath = getenv("PATH");
-	if(executablePath.length()) {
+	std::string oldpath;
+	if(executablePath.size()) {
+		oldpath = getenv("PATH");
 		std::string envPath = fmt::format("{}:{}", oldpath, executablePath);
 		setenv("PATH", envPath.data(), true);
 	}
-	pid_t pid = vfork();
-	if (pid == 0) {
+	auto pid = vfork();
+	if(pid == 0) {
 		execlp("mono", "WindBot.exe", "./WindBot/WindBot.exe",
 			   argPass.data(), argDeck.data(), argPort.data(), argVersion.data(), argName.data(), argChat.data(),
 			   "AssetPath=./WindBot", hand ? argHand.data() : nullptr, nullptr);
 		_exit(EXIT_FAILURE);
 	}
-	setenv("PATH", oldpath.data(), true);
+	if(waitpid(pid, nullptr, WNOHANG) != 0)
+		pid = 0;
+	if(executablePath.size())
+		setenv("PATH", oldpath.data(), true);
 	return pid;
-#endif
 }
+#endif
 
 }
