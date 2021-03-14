44afc67352c42586dc367e8597fea96a42f40475
mercury233 <me@mercury233.me>
2021-03-14 10:33:40 +0800

    select bot deckfile

--- gframe/game.h
+++ gframe/game.h
@@ -25,6 +25,7 @@ struct Config {
 	wchar_t textfont[256];
 	wchar_t numfont[256];
 	wchar_t roompass[20];
+	wchar_t bot_deck_path[64];
 	//settings
 	int chkMAutoPos;
 	int chkSTAutoPos;
@@ -93,6 +94,7 @@ struct BotInfo {
 	bool support_master_rule_3;
 	bool support_new_master_rule;
 	bool support_master_rule_2020;
+	bool select_deckfile;
 };
 
 struct FadingUnit {
@@ -366,6 +368,7 @@ public:
 	irr::gui::IGUIStaticText* stBotInfo;
 	irr::gui::IGUIButton* btnStartBot;
 	irr::gui::IGUIButton* btnBotCancel;
+	irr::gui::IGUIComboBox* cbBotDeck;
 	irr::gui::IGUIComboBox* cbBotRule;
 	irr::gui::IGUICheckBox* chkBotHand;
 	irr::gui::IGUICheckBox* chkBotNoCheckDeck;
