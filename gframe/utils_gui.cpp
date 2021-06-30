9c33c4a50d75932c6f6f04af181b7209a0bba415
Edoardo Lolletti <edoardo762@gmail.com>
2021-06-30 22:03:30 +0200

    Use bundled opengles 2 shaders on android

--- gframe/utils_gui.cpp
+++ gframe/utils_gui.cpp
@@ -93,6 +93,7 @@ irr::IrrlichtDevice* GUIUtils::CreateDevice(GameConfig* configs) {
 		params.DriverType = irr::video::EDT_OPENGL;
 	params.WindowSize = irr::core::dimension2d<irr::u32>((irr::u32)(1024 * configs->dpi_scale), (irr::u32)(640 * configs->dpi_scale));
 #else
+	params.OGLES2ShaderPath = EPRO_TEXT("BUNDLED");
 	if(gGameConfig->use_d3d)
 		params.DriverType = irr::video::EDT_OGLES2;
 	else
