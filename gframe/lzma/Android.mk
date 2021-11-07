824a128b127c3e42f64b4d8985102581bc3244a3
Edoardo Lolletti <edoardo762@gmail.com>
2021-11-07 22:24:37 +0100

    Add prebuilt android lzma
    Built with android ndk 21d

--- gframe/lzma/Android.mk
+++ gframe/lzma/Android.mk
@@ -1,14 +1,9 @@
-# File: Android.mk
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := clzma
-LOCAL_SRC_FILES := Alloc.c \
-                   LzFind.c \
-                   LzmaDec.c \
-                   LzmaEnc.c \
-                   LzmaLib.c \
-                   
-LOCAL_CFLAGS    := -O2
-include $(BUILD_STATIC_LIBRARY)
+LOCAL_SRC_FILES := ./lib/$(TARGET_ARCH_ABI)/libclzma.a
+
+include $(PREBUILT_STATIC_LIBRARY)
 
