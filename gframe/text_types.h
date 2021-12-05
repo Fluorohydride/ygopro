00228c04c205a6e17b8bd36994050c3de01ee107
Edoardo Lolletti <edoardo762@gmail.com>
2021-12-05 20:44:53 +0100

    Minor cleanup in text_types.h

--- gframe/text_types.h
+++ gframe/text_types.h
@@ -31,19 +31,14 @@ inline fmt::basic_string_view<T> to_string_view(const irr::core::string<T, TAllo
 #define STR(x) STR_HELPER(x)
 namespace epro {
 #ifdef UNICODE
-#ifndef EPRO_TEXT
 #define EPRO_TEXT(x) L##x
-#endif
 using path_char = wchar_t;
 #else
-#ifndef EPRO_TEXT
 #define EPRO_TEXT(x) x
-#endif
 using path_char = char;
 #endif // UNICODE
 using path_string = std::basic_string<path_char>;
-template<typename T>
-using basic_string_view = nonstd::basic_string_view<T>;
+using nonstd::basic_string_view;
 using path_stringview = basic_string_view<path_char>;
 using stringview = basic_string_view<char>;
 using wstringview = basic_string_view<wchar_t>;
