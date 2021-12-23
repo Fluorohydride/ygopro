3f543db52a2d60f643c050e00d66e89a8cb2269c
mercury233 <me@mercury233.me>
2021-12-23 15:45:33 +0800

    fix & update loading database

--- gframe/data_manager.cpp
+++ gframe/data_manager.cpp
@@ -62,11 +62,7 @@ bool DataManager::LoadDB(const wchar_t* wfile) {
 			cd.race = sqlite3_column_int(pStmt, 8);
 			cd.attribute = sqlite3_column_int(pStmt, 9);
 			cd.category = sqlite3_column_int(pStmt, 10);
-			auto it = _datas.find(cd.code);
-			if(it != _datas.end())
-				it->second = cd;
-			else
-				_datas.insert(std::make_pair(cd.code, cd));
+			_datas[cd.code] = cd;
 			if(const char* text = (const char*)sqlite3_column_text(pStmt, 12)) {
 				BufferIO::DecodeUTF8(text, strBuffer);
 				cs.name = strBuffer;
@@ -81,7 +77,7 @@ bool DataManager::LoadDB(const wchar_t* wfile) {
 					cs.desc[i] = strBuffer;
 				}
 			}
-			_strings.emplace(cd.code, cs);
+			_strings[cd.code] = cs;
 		}
 	} while(step != SQLITE_DONE);
 	sqlite3_finalize(pStmt);
