3d7b042de468251121be87124f58212b0ac864ae
Edoardo Lolletti <edoardo762@gmail.com>
2022-03-04 19:30:09 +0100

    Fix crash when output downloaded image didn't create properly

--- gframe/image_downloader.cpp
+++ gframe/image_downloader.cpp
@@ -150,9 +150,11 @@ void ImageDownloader::DownloadPic() {
 			if(src.type != type)
 				continue;
 			auto fp = fileopen(name.data(), "wb");
-			if(fp == nullptr && gGameConfig->logDownloadErrors) {
-				ygo::ErrorLog("Failed opening {} for write.", Utils::ToUTF8IfNeeded(name));
-				ygo::ErrorLog("Error: {}.", strerror(errno));
+			if(fp == nullptr) {
+				if(gGameConfig->logDownloadErrors) {
+					ygo::ErrorLog("Failed opening {} for write.", Utils::ToUTF8IfNeeded(name));
+					ygo::ErrorLog("Error: {}.", strerror(errno));
+				}
 				continue;
 			}
 			SetPayloadAndUrl(fmt::format(src.url, code), fp);
