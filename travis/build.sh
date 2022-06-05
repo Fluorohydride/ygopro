98473a6fa0a28db60d2408fb5d10ee712be38ad0
Edoardo Lolletti <edoardo762@gmail.com>
2022-06-05 12:49:15 +0200

    No longer clone ocgcore submodule in actions
    Use the no-core premake option

--- travis/build.sh
+++ travis/build.sh
@@ -9,15 +9,15 @@ DISCORD_APP_ID=${DISCORD_APP_ID:-""}
 UPDATE_URL=${UPDATE_URL:-""}
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    ./premake5 vs2017 --oldwindows=true --sound=sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
+    ./premake5 vs2017 --no-core=true --oldwindows=true --sound=sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
     msbuild.exe -m -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 ./build/ygo.sln -t:ygoprodll -verbosity:minimal -p:EchoOff=true
     python.exe ./travis/patcher.py bin/$BUILD_CONFIG/ygoprodll.exe
 fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    ./premake5 gmake2 --vcpkg-root=$VCPKG_ROOT --sound=sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
+    ./premake5 gmake2 --no-core=true --vcpkg-root=$VCPKG_ROOT --sound=sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
     make -Cbuild -j2 config=$BUILD_CONFIG ygoprodll
 fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    ./premake5 gmake2 --vcpkg-root=$VCPKG_ROOT --sound=sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
+    ./premake5 gmake2 --no-core=true --vcpkg-root=$VCPKG_ROOT --sound=sfml --no-joystick=true --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --covers=\"$COVERS_URL\" --discord=\"$DISCORD_APP_ID\" --update-url=\"$UPDATE_URL\"
     make -Cbuild -j2 config=$BUILD_CONFIG ygoprodll
 fi
