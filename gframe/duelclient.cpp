12a26bec5bd66aa70ea292e6787aa4cb59da063c
mercury233 <me@mercury233.me>
2022-08-12 16:44:17 +0800

    disable BP M2 EP buttons on showing menu

--- gframe/duelclient.cpp
+++ gframe/duelclient.cpp
@@ -872,7 +872,7 @@ int DuelClient::ClientAnalyze(char * msg, unsigned int len) {
 		memcpy(last_successful_msg, msg, len);
 		last_successful_msg_length = len;
 	}
-	mainGame->wCmdMenu->setVisible(false);
+	mainGame->dField.HideMenu();
 	if(!mainGame->dInfo.isReplay && mainGame->dInfo.curMsg != MSG_WAITING && mainGame->dInfo.curMsg != MSG_CARD_SELECTED) {
 		mainGame->waitFrame = -1;
 		mainGame->stHintMsg->setVisible(false);
