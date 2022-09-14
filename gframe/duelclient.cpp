7171206656d114d4163182f309610c750a5d9b5c
Edoardo Lolletti <edoardo762@gmail.com>
2022-09-14 02:45:36 +0200

    Support multple lan clients from the same IP (#2452)
    * Support multiple lam rooms from the same ip on different ports
    
    * Make broadcast socket use SO_REUSEADDR
    
    This allows multiple instances of the client to listen from broadcast messages at the same time on the same port
    
    * Update duelclient.cpp

--- gframe/duelclient.cpp
+++ gframe/duelclient.cpp
@@ -34,7 +34,7 @@ mt19937 DuelClient::rnd;
 bool DuelClient::is_refreshing = false;
 int DuelClient::match_kill = 0;
 std::vector<HostPacket> DuelClient::hosts;
-std::set<unsigned int> DuelClient::remotes;
+std::set<std::pair<unsigned int, unsigned short>> DuelClient::remotes;
 event* DuelClient::resp_event = 0;
 
 bool DuelClient::StartClient(unsigned int ip, unsigned short port, bool create_game) {
@@ -4031,11 +4031,16 @@ void DuelClient::BroadcastReply(evutil_socket_t fd, short events, void * arg) {
 		socklen_t sz = sizeof(sockaddr_in);
 		char buf[256];
 		/*int ret = */recvfrom(fd, buf, 256, 0, (sockaddr*)&bc_addr, &sz);
-		unsigned int ipaddr = bc_addr.sin_addr.s_addr;
 		HostPacket* pHP = (HostPacket*)buf;
-		if(!is_closing && pHP->identifier == NETWORK_SERVER_ID && pHP->version == PRO_VERSION && remotes.find(ipaddr) == remotes.end() ) {
+		if(is_closing || pHP->identifier != NETWORK_SERVER_ID)
+			return;
+		if(pHP->version != PRO_VERSION)
+			return;
+		unsigned int ipaddr = bc_addr.sin_addr.s_addr;
+		const auto remote = std::make_pair(ipaddr, pHP->port);
+		if(remotes.find(remote) == remotes.end()) {
 			mainGame->gMutex.lock();
-			remotes.insert(ipaddr);
+			remotes.insert(remote);
 			pHP->ipaddr = ipaddr;
 			hosts.push_back(*pHP);
 			std::wstring hoststr;
