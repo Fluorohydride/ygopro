734cb82b098dfe56b1ecdf02492b1d17e860bb1f
mercury233 <me@mercury233.me>
2022-09-23 08:48:14 +0800

    minor change declare order

--- gframe/data_manager.cpp
+++ gframe/data_manager.cpp
@@ -13,7 +13,6 @@ DataManager dataManager;
 
 bool DataManager::LoadDB(const wchar_t* wfile) {
 	char file[256];
-	wchar_t strBuffer[4096];
 	BufferIO::EncodeUTF8(wfile, file);
 #ifdef YGOPRO_SERVER_MODE
 	sqlite3* pDB;
@@ -53,6 +52,7 @@ bool DataManager::LoadDB(const wchar_t* wfile) {
 #endif
 	CardDataC cd;
 	CardString cs;
+	wchar_t strBuffer[4096];
 	int step = 0;
 	do {
 		step = sqlite3_step(pStmt);
