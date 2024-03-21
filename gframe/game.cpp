76776deaade86d4a9e5c7b0ff60ec4520107ea08
salix5 <salix5566@gmail.com>
2024-03-22 03:24:49 +0800

    class Game: fix member access

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -2113,18 +2113,18 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 		switch (event.GUIEvent.EventType) {
 		case irr::gui::EGET_ELEMENT_HOVERED: {
 			if (event.GUIEvent.Caller->getType() == EGUIET_EDIT_BOX) {
-				mainGame->SetCursor(event.GUIEvent.Caller->isEnabled() ? ECI_IBEAM : ECI_NORMAL);
+				SetCursor(event.GUIEvent.Caller->isEnabled() ? ECI_IBEAM : ECI_NORMAL);
 				return true;
 			}
-			if (event.GUIEvent.Caller == mainGame->imgCard && mainGame->is_building && !mainGame->is_siding) {
-				mainGame->SetCursor(ECI_HAND);
+			if (event.GUIEvent.Caller == imgCard && is_building && !is_siding) {
+				SetCursor(ECI_HAND);
 				return true;
 			}
 			break;
 		}
 		case irr::gui::EGET_ELEMENT_LEFT: {
-			if (event.GUIEvent.Caller->getType() == EGUIET_EDIT_BOX || event.GUIEvent.Caller == mainGame->imgCard) {
-				mainGame->SetCursor(ECI_NORMAL);
+			if (event.GUIEvent.Caller->getType() == EGUIET_EDIT_BOX || event.GUIEvent.Caller == imgCard) {
+				SetCursor(ECI_NORMAL);
 				return true;
 			}
 			break;
@@ -2133,28 +2133,28 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 			switch (id) {
 			case BUTTON_CLEAR_LOG: {
 				soundManager.PlaySoundEffect(SOUND_BUTTON);
-				mainGame->lstLog->clear();
-				mainGame->logParam.clear();
+				lstLog->clear();
+				logParam.clear();
 				return true;
 				break;
 			}
 			case BUTTON_WINDOW_RESIZE_S: {
-				mainGame->SetWindowsScale(0.8f);
+				SetWindowsScale(0.8f);
 				return true;
 				break;
 			}
 			case BUTTON_WINDOW_RESIZE_M: {
-				mainGame->SetWindowsScale(1.0f);
+				SetWindowsScale(1.0f);
 				return true;
 				break;
 			}
 			case BUTTON_WINDOW_RESIZE_L: {
-				mainGame->SetWindowsScale(1.25f);
+				SetWindowsScale(1.25f);
 				return true;
 				break;
 			}
 			case BUTTON_WINDOW_RESIZE_XL: {
-				mainGame->SetWindowsScale(1.5f);
+				SetWindowsScale(1.5f);
 				return true;
 				break;
 			}
@@ -2164,61 +2164,61 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 		case irr::gui::EGET_CHECKBOX_CHANGED: {
 			switch (id) {
 			case CHECKBOX_AUTO_SEARCH: {
-				mainGame->gameConf.auto_search_limit = mainGame->chkAutoSearch->isChecked() ? 0 : -1;
-				if (mainGame->is_building && !mainGame->is_siding)
-					mainGame->deckBuilder.InstantSearch();
+				gameConf.auto_search_limit = chkAutoSearch->isChecked() ? 0 : -1;
+				if (is_building && !is_siding)
+					deckBuilder.InstantSearch();
 				return true;
 				break;
 			}
 			case CHECKBOX_MULTI_KEYWORDS: {
-				mainGame->gameConf.search_multiple_keywords = mainGame->chkMultiKeywords->isChecked() ? 1 : 0;
-				if (mainGame->is_building && !mainGame->is_siding)
-					mainGame->deckBuilder.InstantSearch();
+				gameConf.search_multiple_keywords = chkMultiKeywords->isChecked() ? 1 : 0;
+				if (is_building && !is_siding)
+					deckBuilder.InstantSearch();
 				return true;
 				break;
 			}
 			case CHECKBOX_ENABLE_MUSIC: {
-				if (!mainGame->chkEnableMusic->isChecked())
+				if (!chkEnableMusic->isChecked())
 					soundManager.StopBGM();
 				return true;
 				break;
 			}
 			case CHECKBOX_DISABLE_CHAT: {
-				bool show = mainGame->is_building ? false : !mainGame->chkIgnore1->isChecked();
-				mainGame->wChat->setVisible(show);
+				bool show = is_building ? false : !chkIgnore1->isChecked();
+				wChat->setVisible(show);
 				if (!show)
-					mainGame->ClearChatMsg();
+					ClearChatMsg();
 				return true;
 				break;
 			}
 			case CHECKBOX_QUICK_ANIMATION: {
-				mainGame->gameConf.quick_animation = mainGame->chkQuickAnimation->isChecked() ? 1 : 0;
+				gameConf.quick_animation = chkQuickAnimation->isChecked() ? 1 : 0;
 				return true;
 				break;
 			}
 			case CHECKBOX_DRAW_SINGLE_CHAIN: {
-				mainGame->gameConf.draw_single_chain = mainGame->chkDrawSingleChain->isChecked() ? 1 : 0;
+				gameConf.draw_single_chain = chkDrawSingleChain->isChecked() ? 1 : 0;
 				return true;
 				break;
 			}
 			case CHECKBOX_HIDE_PLAYER_NAME: {
-				mainGame->gameConf.hide_player_name = mainGame->chkHidePlayerName->isChecked() ? 1 : 0;
-				if (mainGame->gameConf.hide_player_name)
-					mainGame->ClearChatMsg();
+				gameConf.hide_player_name = chkHidePlayerName->isChecked() ? 1 : 0;
+				if (gameConf.hide_player_name)
+					ClearChatMsg();
 				return true;
 				break;
 			}
 			case CHECKBOX_PREFER_EXPANSION: {
-				mainGame->gameConf.prefer_expansion_script = mainGame->chkPreferExpansionScript->isChecked() ? 1 : 0;
+				gameConf.prefer_expansion_script = chkPreferExpansionScript->isChecked() ? 1 : 0;
 				return true;
 				break;
 			}
 			case CHECKBOX_LFLIST: {
-				mainGame->gameConf.use_lflist = mainGame->chkLFlist->isChecked() ? 1 : 0;
-				mainGame->cbLFlist->setEnabled(mainGame->gameConf.use_lflist);
-				mainGame->cbLFlist->setSelected(mainGame->gameConf.use_lflist ? mainGame->gameConf.default_lflist : mainGame->cbLFlist->getItemCount() - 1);
-				mainGame->cbHostLFlist->setSelected(mainGame->gameConf.use_lflist ? mainGame->gameConf.default_lflist : mainGame->cbHostLFlist->getItemCount() - 1);
-				mainGame->deckBuilder.filterList = &deckManager._lfList[mainGame->cbLFlist->getSelected()].content;
+				gameConf.use_lflist = chkLFlist->isChecked() ? 1 : 0;
+				cbLFlist->setEnabled(gameConf.use_lflist);
+				cbLFlist->setSelected(gameConf.use_lflist ? gameConf.default_lflist : cbLFlist->getItemCount() - 1);
+				cbHostLFlist->setSelected(gameConf.use_lflist ? gameConf.default_lflist : cbHostLFlist->getItemCount() - 1);
+				deckBuilder.filterList = &deckManager._lfList[cbLFlist->getSelected()].content;
 				return true;
 				break;
 			}
@@ -2228,9 +2228,9 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 		case irr::gui::EGET_COMBO_BOX_CHANGED: {
 			switch (id) {
 			case COMBOBOX_LFLIST: {
-				mainGame->gameConf.default_lflist = mainGame->cbLFlist->getSelected();
-				mainGame->cbHostLFlist->setSelected(mainGame->gameConf.default_lflist);
-				mainGame->deckBuilder.filterList = &deckManager._lfList[mainGame->gameConf.default_lflist].content;
+				gameConf.default_lflist = cbLFlist->getSelected();
+				cbHostLFlist->setSelected(gameConf.default_lflist);
+				deckBuilder.filterList = &deckManager._lfList[gameConf.default_lflist].content;
 				return true;
 				break;
 			}
@@ -2240,9 +2240,9 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 		case irr::gui::EGET_LISTBOX_CHANGED: {
 			switch (id) {
 			case LISTBOX_LOG: {
-				int sel = mainGame->lstLog->getSelected();
-				if (sel != -1 && (int)mainGame->logParam.size() >= sel && mainGame->logParam[sel]) {
-					mainGame->ShowCardInfo(mainGame->logParam[sel]);
+				int sel = lstLog->getSelected();
+				if (sel != -1 && (int)logParam.size() >= sel && logParam[sel]) {
+					ShowCardInfo(logParam[sel]);
 				}
 				return true;
 				break;
@@ -2253,9 +2253,9 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 		case irr::gui::EGET_LISTBOX_SELECTED_AGAIN: {
 			switch (id) {
 			case LISTBOX_LOG: {
-				int sel = mainGame->lstLog->getSelected();
-				if (sel != -1 && (int)mainGame->logParam.size() >= sel && mainGame->logParam[sel]) {
-					mainGame->wInfos->setActiveTab(0);
+				int sel = lstLog->getSelected();
+				if (sel != -1 && (int)logParam.size() >= sel && logParam[sel]) {
+					wInfos->setActiveTab(0);
 				}
 				return true;
 				break;
@@ -2266,32 +2266,32 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 		case irr::gui::EGET_SCROLL_BAR_CHANGED: {
 			switch (id) {
 			case SCROLL_CARDTEXT: {
-				if (!mainGame->scrCardText->isVisible()) {
+				if (!scrCardText->isVisible()) {
 					return true;
 					break;
 				}
-				u32 pos = mainGame->scrCardText->getPos();
-				mainGame->SetStaticText(mainGame->stText, mainGame->stText->getRelativePosition().getWidth() - 25, mainGame->guiFont, mainGame->showingtext, pos);
+				u32 pos = scrCardText->getPos();
+				SetStaticText(stText, stText->getRelativePosition().getWidth() - 25, guiFont, showingtext, pos);
 				return true;
 				break;
 			}
 			case SCROLL_VOLUME: {
-				mainGame->gameConf.sound_volume = (double)mainGame->scrSoundVolume->getPos() / 100;
-				mainGame->gameConf.music_volume = (double)mainGame->scrMusicVolume->getPos() / 100;
-				soundManager.SetSoundVolume(mainGame->gameConf.sound_volume);
-				soundManager.SetMusicVolume(mainGame->gameConf.music_volume);
+				gameConf.sound_volume = (double)scrSoundVolume->getPos() / 100;
+				gameConf.music_volume = (double)scrMusicVolume->getPos() / 100;
+				soundManager.SetSoundVolume(gameConf.sound_volume);
+				soundManager.SetMusicVolume(gameConf.music_volume);
 				return true;
 				break;
 			}
 			case SCROLL_TAB_HELPER: {
-				rect<s32> pos = mainGame->tabHelper->getRelativePosition();
-				mainGame->tabHelper->setRelativePosition(recti(0, mainGame->scrTabHelper->getPos() * -1, pos.LowerRightCorner.X, pos.LowerRightCorner.Y));
+				rect<s32> pos = tabHelper->getRelativePosition();
+				tabHelper->setRelativePosition(recti(0, scrTabHelper->getPos() * -1, pos.LowerRightCorner.X, pos.LowerRightCorner.Y));
 				return true;
 				break;
 			}
 			case SCROLL_TAB_SYSTEM: {
-				rect<s32> pos = mainGame->tabSystem->getRelativePosition();
-				mainGame->tabSystem->setRelativePosition(recti(0, mainGame->scrTabSystem->getPos() * -1, pos.LowerRightCorner.X, pos.LowerRightCorner.Y));
+				rect<s32> pos = tabSystem->getRelativePosition();
+				tabSystem->setRelativePosition(recti(0, scrTabSystem->getPos() * -1, pos.LowerRightCorner.X, pos.LowerRightCorner.Y));
 				return true;
 				break;
 			}
@@ -2301,14 +2301,14 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 		case irr::gui::EGET_EDITBOX_ENTER: {
 			switch (id) {
 			case EDITBOX_CHAT: {
-				if (mainGame->dInfo.isReplay)
+				if (dInfo.isReplay)
 					break;
-				const wchar_t* input = mainGame->ebChatInput->getText();
+				const wchar_t* input = ebChatInput->getText();
 				if (input[0]) {
 					unsigned short msgbuf[256];
 					int len = BufferIO::CopyWStr(input, msgbuf, 256);
 					DuelClient::SendBufferToServer(CTOS_CHAT, msgbuf, (len + 1) * sizeof(short));
-					mainGame->ebChatInput->setText(L"");
+					ebChatInput->setText(L"");
 					return true;
 				}
 				break;
@@ -2323,26 +2323,26 @@ bool Game::OnCommonEvent(const irr::SEvent& event) {
 	case irr::EET_KEY_INPUT_EVENT: {
 		switch (event.KeyInput.Key) {
 		case irr::KEY_KEY_R: {
-			if (mainGame->gameConf.control_mode == 0
-				&& !event.KeyInput.PressedDown && !mainGame->HasFocus(EGUIET_EDIT_BOX)) {
-				mainGame->textFont->setTransparency(true);
-				mainGame->guiFont->setTransparency(true);
+			if (gameConf.control_mode == 0
+				&& !event.KeyInput.PressedDown && !HasFocus(EGUIET_EDIT_BOX)) {
+				textFont->setTransparency(true);
+				guiFont->setTransparency(true);
 			}
 			return true;
 			break;
 		}
 		case irr::KEY_F9: {
-			if (mainGame->gameConf.control_mode == 1
-				&& !event.KeyInput.PressedDown && !mainGame->HasFocus(EGUIET_EDIT_BOX)) {
-				mainGame->textFont->setTransparency(true);
-				mainGame->guiFont->setTransparency(true);
+			if (gameConf.control_mode == 1
+				&& !event.KeyInput.PressedDown && !HasFocus(EGUIET_EDIT_BOX)) {
+				textFont->setTransparency(true);
+				guiFont->setTransparency(true);
 			}
 			return true;
 			break;
 		}
 		case irr::KEY_ESCAPE: {
-			if (!mainGame->HasFocus(EGUIET_EDIT_BOX))
-				mainGame->device->minimizeWindow();
+			if (!HasFocus(EGUIET_EDIT_BOX))
+				device->minimizeWindow();
 			return true;
 			break;
 		}
