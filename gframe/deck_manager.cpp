790252e650e4670bc7343889fede8d9ce4e23726
salix5 <salix5566@gmail.com>
2024-06-18 16:54:59 +0800

    check sizeof deckBuffer in LoadDeck (#2564)
    * Revert "fix"
    
    This reverts commit 51aa89407bc107ede4bc9b56c97002fa9a7edca6.
    
    * check sizeof deckBuffer in LoadDeck
    
    * clear current_deck when selecting a wrong file

--- gframe/deck_manager.cpp
+++ gframe/deck_manager.cpp
@@ -242,6 +242,8 @@ bool DeckManager::LoadDeck(irr::gui::IGUIComboBox* cbCategory, irr::gui::IGUICom
 	bool res = LoadDeck(filepath, is_packlist);
 	if(res && mainGame->is_building)
 		mainGame->deckBuilder.RefreshPackListScroll();
+	if (!res)
+		current_deck.clear();
 	return res;
 }
 FILE* DeckManager::OpenDeckFile(const wchar_t* file, const char* mode) {
@@ -280,8 +282,8 @@ bool DeckManager::LoadDeck(const wchar_t* file, bool is_packlist) {
 	}
 	if(!reader)
 		return false;
-	size_t size = reader->getSize();
-	if(size >= 0x20000) {
+	auto size = reader->getSize();
+	if(size >= (int)sizeof deckBuffer) {
 		reader->drop();
 		return false;
 	}
@@ -296,7 +298,7 @@ bool DeckManager::LoadDeck(std::istringstream* deckStream, bool is_packlist) {
 	int cardlist[300];
 	bool is_side = false;
 	std::string linebuf;
-	while(std::getline(*deckStream, linebuf) && ct < 300) {
+	while(std::getline(*deckStream, linebuf, '\n') && ct < 300) {
 		if(linebuf[0] == '!') {
 			is_side = true;
 			continue;
