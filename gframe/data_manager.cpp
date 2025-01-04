878f12ab31bbe6c402a8bbb8ab0094997de4c265
salix5 <salix5566@gmail.com>
2025-01-04 19:02:42 +0800

    fix ScriptReader

--- gframe/data_manager.cpp
+++ gframe/data_manager.cpp
@@ -424,14 +424,11 @@ unsigned char* DataManager::ScriptReader(const char* script_name, int* slen) {
 #endif
 	if (!reader)
 		return nullptr;
-	size_t size = reader->getSize();
-	if (size > sizeof scriptBuffer) {
-		reader->drop();
-		return nullptr;
-	}
-	reader->read(scriptBuffer, size);
+	int size = reader->read(scriptBuffer, sizeof scriptBuffer);
 	reader->drop();
-	*slen = (int)size;
+	if (size >= (int)sizeof scriptBuffer)
+		return nullptr;
+	*slen = size;
 	return scriptBuffer;
 }
 unsigned char* DataManager::DefaultScriptReader(const char* script_name, int* slen) {
