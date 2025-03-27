5ebd8aefc9b4332df828886efc90e17ef1d4c2a7
purerosefallen <78877@qq.com>
2025-03-27 11:55:44 +0800

    update miniaudio and stb_vorbis.c

--- gframe/miniaudio/premake5.lua
+++ gframe/miniaudio/premake5.lua
@@ -2,11 +2,5 @@ project "cminiaudio"
     kind "StaticLib"
     files { "*.c", "*.h" }
 
-    filter "action:vs*"
-        forceincludes { "stb_vorbis.h" }
-
-    filter "not action:vs*"
-        forceincludes { "./stb_vorbis.h" }
-
     filter "system:linux"
         links { "dl", "pthread", "m" }
