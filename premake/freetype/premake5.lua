2361c36cb394d47df9690bfc2aefbdf45c0819f2
Nanahira <78877@qq.com>
2025-03-28 10:49:22 +0800

    remove unneeded files from freetype
    Using automated testing tool.

--- premake/freetype/premake5.lua
+++ premake/freetype/premake5.lua
@@ -6,25 +6,9 @@ project "freetype"
 
     files { "src/autofit/autofit.c",
             "src/base/ftbase.c",
-            "src/base/ftbbox.c",
-            "src/base/ftbdf.c",
             "src/base/ftbitmap.c",
-            "src/base/ftcid.c",
-            "src/base/ftfstype.c",
-            "src/base/ftgasp.c",
-            "src/base/ftglyph.c",
-            "src/base/ftgxval.c",
             "src/base/ftinit.c",
-            "src/base/ftmm.c",
-            "src/base/ftotval.c",
-            "src/base/ftpatent.c",
-            "src/base/ftpfr.c",
-            "src/base/ftstroke.c",
-            "src/base/ftsynth.c",
-            "src/base/fttype1.c",
-            "src/base/ftwinfnt.c",
             "src/bdf/bdf.c",
-            "src/cache/ftcache.c",
             "src/cff/cff.c",
             "src/cid/type1cid.c",
             "src/gzip/ftgzip.c",
@@ -42,6 +26,7 @@ project "freetype"
             "src/type1/type1.c",
             "src/type42/type42.c",
             "src/winfonts/winfnt.c" }
+
 
     if os.isfile("src/svg/svg.c") then
         files { "src/svg/svg.c" }
