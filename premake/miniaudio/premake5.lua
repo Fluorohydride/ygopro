845c92d4cbc50e725404db423ac9c67c47eba8bb
mercury233 <me@mercury233.me>
2025-05-06 20:36:09 +0800

    add OP_HAVE_LRINTF

--- premake/miniaudio/premake5.lua
+++ premake/miniaudio/premake5.lua
@@ -118,7 +118,7 @@ project "miniaudio"
                 "external/vorbis/include",
             }
             defines {
-                "OPUS_BUILD", "USE_ALLOCA",
+                "OPUS_BUILD", "USE_ALLOCA", "HAVE_LRINTF", "OP_HAVE_LRINTF",
                 "OPUS_X86_PRESUME_SSE", "OPUS_X86_PRESUME_SSE2",
                 "OPUS_HAVE_RTCD", "OPUS_X86_MAY_HAVE_SSE", "OPUS_X86_MAY_HAVE_SSE4_1", "OPUS_X86_MAY_HAVE_AVX2",
             }
