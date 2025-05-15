a26a8ee39c6e21ac5f3c78d47fb7c4f3a89b5996
mercury233 <me@mercury233.me>
2025-05-15 09:24:34 +0800

    fix

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -2033,7 +2033,9 @@ void Game::OnResize() {
 	btnBigCardZoomIn->setRelativePosition(Resize(205, 140, 295, 175));
 	btnBigCardZoomOut->setRelativePosition(Resize(205, 180, 295, 215));
 	btnBigCardClose->setRelativePosition(Resize(205, 230, 295, 265));
-	env->getSkin()->setSize(irr::gui::EGDS_SCROLLBAR_SIZE, static_cast<irr::s32>(14 * xScale));
+
+	irr::s32 barWidth = (xScale > 1) ? gameConf.textfontsize * xScale : gameConf.textfontsize;
+	env->getSkin()->setSize(irr::gui::EGDS_SCROLLBAR_SIZE, barWidth);
 }
 void Game::ResizeChatInputWindow() {
 	irr::s32 x = wInfos->getRelativePosition().LowerRightCorner.X + 6;
