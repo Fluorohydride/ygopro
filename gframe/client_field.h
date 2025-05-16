fedacbaf546a1ae0dccad5674a384e56119072fc
purerosefallen <78877@qq.com>
2025-05-16 17:00:15 +0800

    display side count to removed

--- gframe/client_field.h
+++ gframe/client_field.h
@@ -95,7 +95,7 @@ public:
 	ClientField();
 	~ClientField();
 	void Clear();
-	void Initial(int player, int deckc, int extrac);
+	void Initial(int player, int deckc, int extrac, int sidec = 0);
 	void ResetSequence(std::vector<ClientCard*>& list, bool reset_height);
 	ClientCard* GetCard(int controler, int location, int sequence, int sub_seq = 0);
 	void AddCard(ClientCard* pcard, int controler, int location, int sequence);
