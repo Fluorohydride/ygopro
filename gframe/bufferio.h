8583ca26ad112adcd3045168c4d3b4dfd9de6264
salix5 <salix5566@gmail.com>
2025-06-13 03:37:49 +0800

    Refactor BufferIO calls to function template

--- gframe/bufferio.h
+++ gframe/bufferio.h
@@ -10,9 +10,6 @@ public:
 	static int32_t ReadInt32(unsigned char*& p) {
 		return buffer_read<int32_t>(p);
 	}
-	static uint16_t ReadUInt16(unsigned char*& p) {
-		return buffer_read<uint16_t>(p);
-	}
 	static uint8_t ReadUInt8(unsigned char*& p) {
 		return buffer_read<uint8_t>(p);
 	}
