f4db57505848a9407238bc24763d195ee61c1b3f
Mercury233 <me@mercury233.me>
2025-06-29 17:39:57 +0800

    update MSG_CONFIRM_CARDS, add skip_panel (#2470)

--- gframe/single_duel.cpp
+++ gframe/single_duel.cpp
@@ -821,6 +821,7 @@ int SingleDuel::Analyze(unsigned char* msgbuffer, unsigned int len) {
 		}
 		case MSG_CONFIRM_CARDS: {
 			player = BufferIO::ReadUInt8(pbuf);
+			pbuf += 1;
 			count = BufferIO::ReadUInt8(pbuf);
 			if(pbuf[5] != LOCATION_DECK) {
 				pbuf += count * 7;
