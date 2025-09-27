c0613b956a29e10841604d0025f73c794ddc557a
purerosefallen <78877@qq.com>
2025-09-27 11:02:21 +0800

    add credit display

--- gframe/image_manager.h
+++ gframe/image_manager.h
@@ -54,6 +54,7 @@ public:
 	irr::video::ITexture* tTarget;
 	irr::video::ITexture* tChainTarget;
 	irr::video::ITexture* tLim;
+	irr::video::ITexture* tLimCredit;
 	irr::video::ITexture* tOT;
 	irr::video::ITexture* tHand[3];
 	irr::video::ITexture* tBackGround;
