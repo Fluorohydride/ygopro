8e98efc8d6ef79bf6239c2b4b7e97112322dbdef
salix5 <salix5566@gmail.com>
2025-12-28 09:51:12 +0800

    Revert "use std::forward" (#2950)

--- gframe/config.h
+++ gframe/config.h
@@ -54,12 +54,12 @@
 #include "../ocgcore/ocgapi.h"
 
 template<size_t N, typename... TR>
-inline int myswprintf(wchar_t(&buf)[N], const wchar_t* fmt, TR&&... args) {
-	return std::swprintf(buf, N, fmt, std::forward<TR>(args)...);
+inline int myswprintf(wchar_t(&buf)[N], const wchar_t* fmt, TR... args) {
+	return std::swprintf(buf, N, fmt, args...);
 }
 template<size_t N, typename... TR>
-inline int mysnprintf(char(&buf)[N], const char* fmt, TR&&... args) {
-	return std::snprintf(buf, N, fmt, std::forward<TR>(args)...);
+inline int mysnprintf(char(&buf)[N], const char* fmt, TR... args) {
+	return std::snprintf(buf, N, fmt, args...);
 }
 
 inline FILE* mywfopen(const wchar_t* filename, const char* mode) {
