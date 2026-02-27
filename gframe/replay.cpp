40f6d72e882b687405539620a1e59a5c723e0124
salix5 <salix5566@gmail.com>
2026-02-27 17:14:49 +0800

    Replay: add SaveDeck

--- gframe/replay.cpp
+++ gframe/replay.cpp
@@ -220,6 +220,14 @@ void Replay::SkipInfo(){
 bool Replay::IsReplaying() const {
 	return is_replaying;
 }
+bool Replay::SaveDeck(size_t index, const wchar_t* filename) {
+	if(index >= decks.size())
+		return false;
+	DeckArray deck = decks[index];
+	std::reverse(deck.main.begin(), deck.main.end());
+	std::reverse(deck.extra.begin(), deck.extra.end());
+	return deckManager.SaveDeckArray(deck, filename);
+}
 bool Replay::ReadInfo() {
 	int player_count = (pheader.base.flag & REPLAY_TAG) ? 4 : 2;
 	for (int i = 0; i < player_count; ++i) {
