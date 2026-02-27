734d1d10c59fbd4daa9935019f8e7b70517299d7
salix5 <salix5566@gmail.com>
2026-02-27 16:35:15 +0800

    Add deck.h

---
+++ gframe/deck.h
@@ -0,0 +1,35 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <vector>
+#include <cstdint>
+#include "data_manager.h"
+
+namespace ygo {
+
+struct Deck {
+	std::vector<code_pointer> main;
+	std::vector<code_pointer> extra;
+	std::vector<code_pointer> side;
+	Deck() = default;
+	Deck(const Deck& ndeck) {
+		main = ndeck.main;
+		extra = ndeck.extra;
+		side = ndeck.side;
+	}
+	void clear() {
+		main.clear();
+		extra.clear();
+		side.clear();
+	}
+};
+
+struct DeckArray {
+	std::vector<uint32_t> main;
+	std::vector<uint32_t> extra;
+	std::vector<uint32_t> side;
+};
+
+}
+
+#endif // DECK_H
