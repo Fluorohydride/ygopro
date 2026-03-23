12e5753b587da44d51e50a61b3d9a9040603348c
mercury233 <me@mercury233.me>
2026-03-23 09:33:27 +0800

    update irrlicht premake

--- premake/irrlicht/premake5.lua
+++ premake/irrlicht/premake5.lua
@@ -172,8 +172,6 @@ project "irrlicht"
     filter { "system:macosx" }
         cppdialect "gnu++14"
         defines { "GL_SILENCE_DEPRECATION" }
-        undefines { "NO_IRR_COMPILE_WITH_JOYSTICK_EVENTS_" }
-        includedirs { "source/Irrlicht" }
         files {
             "source/Irrlicht/*.mm",
         }
