ae7c6f59629d5f9a03f26855dec3d004464a40d1
mercury233 <me@mercury233.me>
2026-04-09 09:35:32 +0800

    use getData

--- gframe/image_utility.cpp
+++ gframe/image_utility.cpp
@@ -200,8 +200,8 @@ irr::video::IImage* ImageUtility::RotateImageCCW90(irr::video::IVideoDriver* dri
 	irr::core::dimension2d<irr::u32> srcSize = src->getDimension();
 	irr::core::dimension2d<irr::u32> destSize(srcSize.Height, srcSize.Width);
 	irr::video::IImage* dest = driver->createImage(src->getColorFormat(), destSize);
-	void* srcData = src->lock();
-	void* destData = dest->lock();
+	void* srcData = src->getData();
+	void* destData = dest->getData();
 	if(srcData && destData) {
 		irr::u32 srcPitch = src->getPitch();
 		irr::u32 destPitch = dest->getPitch();
