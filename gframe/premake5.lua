1cf96585cf5c14f169ec01c2dd521ab9b146bdc5
mercury233 <me@mercury233.me>
2026-04-23 10:08:45 +0800

    remove irrklang

--- gframe/premake5.lua
+++ gframe/premake5.lua
@@ -60,33 +60,13 @@ project "YGOPro"
                 end
             end
         end
-        if AUDIO_LIB == "irrklang" then
-            defines { "YGOPRO_USE_IRRKLANG" }
-            includedirs { IRRKLANG_INCLUDE_DIR }
-            if not IRRKLANG_PRO then
-                libdirs { IRRKLANG_LIB_DIR }
-            end
-            if IRRKLANG_PRO_BUILD_IKPMP3 then
-                links { "ikpmp3" }
-            end
-        end
     end
 
     filter "system:windows"
         entrypoint "mainCRTStartup"
         files "ygopro.rc"
         links { "ws2_32", "iphlpapi", "winmm" }
-        if USE_AUDIO and AUDIO_LIB == "irrklang" then
-            links { "irrKlang" }
-            if IRRKLANG_PRO then
-                defines { "IRRKLANG_STATIC" }
-                filter { "system:windows", "not configurations:Debug" }
-                    libdirs { IRRKLANG_PRO_RELEASE_LIB_DIR }
-                filter { "system:windows", "configurations:Debug" }
-                    libdirs { IRRKLANG_PRO_DEBUG_LIB_DIR }
-                filter {}
-            end
-        end
+
     filter "not action:vs*"
         cppdialect "C++14"
 
@@ -99,16 +79,9 @@ project "YGOPro"
         if MAC_INTEL then
             linkoptions { "-arch x86_64" }
         end
-        if USE_AUDIO and AUDIO_LIB == "irrklang" then
-            links { "irrklang" }
-        end
 
     filter "system:linux"
         links { "GL", "X11", "dl", "pthread" }
         if USE_OPENMP then
             linkoptions { "-fopenmp" }
-        end
-        if USE_AUDIO and AUDIO_LIB == "irrklang" then
-            links { "IrrKlang" }
-            linkoptions{ IRRKLANG_LINK_RPATH }
         end
--- premake5.lua
+++ premake5.lua
@@ -23,13 +23,10 @@ BUILD_JPEG = os.istarget("windows") -- libjpeg-turbo is required, can't use the 
 JPEG_LIB_NAME = "jpeg" -- use the libjpeg API of libjpeg-turbo, the lib name should always be "jpeg", just in case
 
 USE_AUDIO = true
-AUDIO_LIB = "miniaudio" -- can be "miniaudio" or "irrklang"
+AUDIO_LIB = "miniaudio" -- only miniaudio is supported for now
 -- BUILD_MINIAUDIO is always true
 MINIAUDIO_SUPPORT_OPUS_VORBIS = true
 MINIAUDIO_BUILD_OPUS_VORBIS = os.istarget("windows")
--- BUILD_IRRKLANG is impossible because irrKlang is not open source
-IRRKLANG_PRO = false
-IRRKLANG_PRO_BUILD_IKPMP3 = false
 
 -- Default include dirs, those values are ONLY used in static builds, WILL BE IGNORED if set corresponding BUILD_* to false
 LUA_INCLUDE_DIR = path.getabsolute("./lua/src")
@@ -82,7 +79,7 @@ newoption { trigger = "jpeg-include-dir", category = "YGOPro - jpeg", descriptio
 newoption { trigger = "jpeg-lib-dir", category = "YGOPro - jpeg", description = "", value = "PATH" }
 
 newoption { trigger = "no-audio", category = "YGOPro", description = "" }
-newoption { trigger = "audio-lib", category = "YGOPro", description = "", value = "miniaudio, irrklang", default = AUDIO_LIB }
+newoption { trigger = "audio-lib", category = "YGOPro", description = "", value = "", default = AUDIO_LIB }
 
 newoption { trigger = "miniaudio-support-opus-vorbis", category = "YGOPro - miniaudio", description = "" }
 newoption { trigger = "no-miniaudio-support-opus-vorbis", category = "YGOPro - miniaudio", description = "" }
@@ -96,17 +93,6 @@ newoption { trigger = "vorbis-include-dir", category = "YGOPro - miniaudio", des
 newoption { trigger = "vorbis-lib-dir", category = "YGOPro - miniaudio", description = "", value = "PATH" }
 newoption { trigger = "ogg-include-dir", category = "YGOPro - miniaudio", description = "", value = "PATH" }
 newoption { trigger = "ogg-lib-dir", category = "YGOPro - miniaudio", description = "", value = "PATH" }
-
-newoption { trigger = "use-irrklang", category = "YGOPro - irrklang", description = "Deprecated, use audio-lib=irrklang" }
-newoption { trigger = "no-use-irrklang", category = "YGOPro - irrklang", description = "Deprecated, use no-audio" }
-newoption { trigger = "irrklang-include-dir", category = "YGOPro - irrklang", description = "", value = "PATH" }
-newoption { trigger = "irrklang-lib-dir", category = "YGOPro - irrklang", description = "", value = "PATH" }
-
-newoption { trigger = "irrklang-pro", category = "YGOPro - irrklang - pro", description = "" }
-newoption { trigger = "no-irrklang-pro", category = "YGOPro - irrklang - pro", description = "" }
-newoption { trigger = "irrklang-pro-release-lib-dir", category = "YGOPro - irrklang - pro", description = "", value = "PATH" }
-newoption { trigger = "irrklang-pro-debug-lib-dir", category = "YGOPro - irrklang - pro", description = "", value = "PATH" }
-newoption { trigger = 'build-ikpmp3', category = "YGOPro - irrklang - ikpmp3", description = "" }
 
 newoption { trigger = "mac-arm", category = "YGOPro", description = "Compile for Apple Silicon Mac" }
 newoption { trigger = "mac-intel", category = "YGOPro", description = "Compile for Intel Mac" }
@@ -216,13 +202,6 @@ elseif GetParam("use-miniaudio") then
     print("Warning: --use-miniaudio is deprecated, use --audio-lib=miniaudio")
     USE_AUDIO = true
     AUDIO_LIB = "miniaudio"
-elseif GetParam("no-use-irrklang") then
-    print("Warning: --no-use-irrklang is deprecated, use --no-audio")
-    USE_AUDIO = false
-elseif GetParam("use-irrklang") then
-    print("Warning: --use-irrklang is deprecated, use --audio-lib=irrklang")
-    USE_AUDIO = true
-    AUDIO_LIB = "irrklang"
 end
 
 if USE_AUDIO then
@@ -250,29 +229,6 @@ if USE_AUDIO then
                 OGG_LIB_DIR = GetParam("ogg-lib-dir") or os.findlib("ogg")
             end
         end
-    elseif AUDIO_LIB == "irrklang" then
-        print("Warning: irrKlang is deprecated and may be removed in future, please consider switching to miniaudio")
-        IRRKLANG_INCLUDE_DIR = GetParam("irrklang-include-dir") or "../irrklang/include"
-        if os.istarget("windows") then
-            IRRKLANG_LIB_DIR = "../irrklang/lib/Win32-visualStudio"
-        elseif os.istarget("linux") then
-            IRRKLANG_LIB_DIR = "../irrklang/bin/linux-gcc-64"
-            IRRKLANG_LINK_RPATH = "-Wl,-rpath=./irrklang/bin/linux-gcc-64/"
-        elseif os.istarget("macosx") then
-            IRRKLANG_LIB_DIR = "../irrklang/bin/macosx-gcc"
-        end
-        IRRKLANG_LIB_DIR = GetParam("irrklang-lib-dir") or IRRKLANG_LIB_DIR
-        if GetParam("irrklang-pro") and os.istarget("windows") then
-            IRRKLANG_PRO = true
-        elseif GetParam("no-irrklang-pro") then
-            IRRKLANG_PRO = false
-        end
-        if IRRKLANG_PRO then
-            -- irrklang pro can't use the pro lib to debug
-            IRRKLANG_PRO_RELEASE_LIB_DIR = GetParam("irrklang-pro-release-lib-dir") or "../irrklang/lib/Win32-vs2019"
-            IRRKLANG_PRO_DEBUG_LIB_DIR = GetParam("irrklang-pro-debug-lib-dir") or "../irrklang/lib/Win32-visualStudio-debug"
-        end
-        IRRKLANG_PRO_BUILD_IKPMP3 = GetParam("build-ikpmp3") or IRRKLANG_PRO
     else
         error("Unknown audio library: " .. AUDIO_LIB)
     end
@@ -398,8 +354,5 @@ workspace "YGOPro"
     if USE_AUDIO then
         if AUDIO_LIB=="miniaudio" then
             include "miniaudio"
-        end
-        if IRRKLANG_PRO_BUILD_IKPMP3 then
-            include "ikpmp3"
         end
     end
