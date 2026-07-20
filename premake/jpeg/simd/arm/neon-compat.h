8b52ae2ad9225260365131c5236a2e42576a51c0
Mercury233 <me@mercury233.me>
2026-07-20 08:32:29 +0800

    upgrade libevent and libjpeg-turbo and other (#3139)

--- premake/jpeg/simd/arm/neon-compat.h
+++ premake/jpeg/simd/arm/neon-compat.h
@@ -1,9 +1,10 @@
 /* Pre-generated for premake builds.  Assumes a modern compiler (GCC 12+,
-   Clang 3.8+, MSVC 19.20+) that provides the full set of Neon intrinsics.
-   These correspond to -DNEON_INTRINSICS (full intrinsics implementation). */
+   Clang 3.8+, MSVC 19.20+) that provides the full set of Neon intrinsics. */
 #define HAVE_VLD1_S16_X3
 #define HAVE_VLD1_U16_X2
 #define HAVE_VLD1Q_U8_X4
+
+#include <arm_neon.h>
 
 /* Define compiler-independent count-leading-zeros and byte-swap macros */
 #if defined(_MSC_VER) && !defined(__clang__)
