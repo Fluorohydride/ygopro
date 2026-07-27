b2fbf8ee82dcdc09ecba436aa51714e4eb6136e5
salix5 <salix5566@gmail.com>
2026-07-27 20:17:33 +0800

    Include irrlicht/defines.lua in gframe/premake5.lua

--- gframe/premake5.lua
+++ gframe/premake5.lua
@@ -5,7 +5,7 @@ project "YGOPro"
         openmp "On"
     end
 
-    defines { "_IRR_STATIC_LIB_" }
+    dofile("../irrlicht/defines.lua")
     files { "*.cpp", "*.h" }
 
     includedirs { "../ocgcore" }
@@ -59,6 +59,11 @@ project "YGOPro"
         files "ygopro.rc"
         links { "ws2_32", "iphlpapi", "winmm" }
         defines { "NOMINMAX=1", "WIN32_LEAN_AND_MEAN" }
+        if USE_DXSDK then
+            defines { "IRR_COMPILE_WITH_DX9_DEV_PACK" }
+        else
+            defines { "NO_IRR_COMPILE_WITH_DIRECT3D_9_" }
+        end
 
     filter "not action:vs*"
         cppdialect "C++14"
